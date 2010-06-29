484c12463bc08c178565990a2a7728aca066baa1
vdave <dave.vieglais@gmail.com>
2010-06-29 15:10:52 +0000

    Regenerated with version 1.1.2, renamed 'generated' to 'types'

---
+++ d1_common_python/src/d1common/types/__init__.py
@@ -0,0 +1,4 @@
+'''
+Code living in this folder will be destroyed when regenerating from the schemas.
+Don't manually edit content in this folder.
+'''
--- d1_common_python/src/generate-from-schema.sh
+++ d1_common_python/src/generate-from-schema.sh
@@ -3,11 +3,12 @@
 # Depends on PyXB ( http://pyxb.sourceforge.net/userref_pyxbgen.html )
 #
 # 20100628 - Dave Vieglais for DataONE.org
-# Execute this script in the src folder of the distribution.
+# Execute this script in the src folder of the distribution after any
+# changes to the xmlschema definitions.
 
 
 BASEURL="https://repository.dataone.org/software/cicore/trunk/schemas"
-pyxbgen --binding-root d1common/generated \
+pyxbgen --binding-root d1common/types \
         -u $BASEURL/logging.xsd -m logging \
         -u $BASEURL/noderegistry.xsd -m noderegistry \
         -u $BASEURL/objectlist.xsd -m objectlist \
