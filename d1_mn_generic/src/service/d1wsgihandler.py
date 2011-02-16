31e3297b5f6fc63c7d059dbc116d8723102ccfd8
Roger Dahl <github@dahlsys.com>
2011-02-16 16:58:08 +0000

    d1wsgihandler updated to work with Django 1.2.5 final.

--- d1_mn_generic/src/service/d1wsgihandler.py
+++ d1_mn_generic/src/service/d1wsgihandler.py
@@ -10,33 +10,34 @@ class D1WSGIRequest(django.core.handlers.wsgi.WSGIRequest):
 
   def _load_post_and_files(self):
     # Populates self._post and self._files
-    if self.method not in ['POST', 'PUT']:
+    if self.method in ('POST', 'PUT'):
+      if self.environ.get('CONTENT_TYPE', '').startswith('multipart'):
+        self._raw_post_data = ''
+        try:
+          self._post, self._files = self.parse_file_upload(
+            self.META, self.environ['wsgi.input']
+          )
+        except:
+          # An error occured while parsing POST data.  Since when
+          # formatting the error the request handler might access
+          # self.POST, set self._post and self._file to prevent
+          # attempts to parse POST data again.
+          self._post = http.QueryDict('')
+          self._files = datastructures.MultiValueDict()
+          # Mark that an error occured.  This allows self.__repr__ to
+          # be explicit about it instead of simply representing an
+          # empty POST
+          self._post_parse_error = True
+          raise
+      else:
+        self._post, self._files = http.QueryDict(
+          self.raw_post_data, encoding=self._encoding
+        ), datastructures.MultiValueDict(
+        )
+    else:
       self._post, self._files = http.QueryDict('', encoding=self._encoding
                                                ), datastructures.MultiValueDict(
                                                )
-      return
-    if self._read_started:
-      self._mark_post_parse_error()
-      return
-    if self.META.get('CONTENT_TYPE', '').startswith('multipart'):
-      self._raw_post_data = ''
-      try:
-        self._post, self._files = self.parse_file_upload(self.META, self)
-      except:
-        # An error occured while parsing POST data.  Since when
-        # formatting the error the request handler might access
-        # self.POST, set self._post and self._file to prevent
-        # attempts to parse POST data again.
-        # Mark that an error occured.  This allows self.__repr__ to
-        # be explicit about it instead of simply representing an
-        # empty POST
-        self._mark_post_parse_error()
-        raise
-    else:
-      self._post, self._files = http.QueryDict(
-        self.raw_post_data, encoding=self._encoding
-      ), datastructures.MultiValueDict(
-      )
 
 
 class D1WSGIHandler(django.core.handlers.wsgi.WSGIHandler):
