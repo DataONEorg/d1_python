b6bcdb41716a55895e0ec18673ab9d088358f919
Roger Dahl <git@dahlsys.com>
2011-03-24 15:44:34 +0000

    - Changed ‘application/octet-stream’ mimetype literals to constant defined in d1_common.const. - Checksum serialization tests moved from GMN integration test.

--- d1_common_python/src/tests/test_checksum.py
+++ d1_common_python/src/tests/test_checksum.py
@@ -35,6 +35,7 @@ import logging
 import unittest
 from d1_common import xmlrunner
 from d1_common.types import checksum_serialization
+import d1_common.const
 
 EG_CHECKSUM_GMN = (
   """<?xml version="1.0" ?><ns1:checksum algorithm="SHA-1" xmlns:ns1="http://dataone.org/service/types/0.5.1">3f56de593b6ffc536253b799b429453e3673fc19</ns1:checksum>""",
@@ -56,26 +57,53 @@ EG_BAD_CHECKSUM_2 = (
 
 
 class TestChecksum(unittest.TestCase):
-  def test_serialization(self):
+  def doctest(self, doc, shouldfail=False):
     loader = checksum_serialization.Checksum('dummy')
-
-    def doctest(doc, shouldfail=False):
-      try:
-        checksum = loader.deserialize(doc[0], content_type="text/xml")
-      except:
-        if shouldfail:
-          pass
-        else:
-          raise
+    try:
+      checksum = loader.deserialize(doc[0], content_type="text/xml")
+    except:
+      if shouldfail:
+        pass
       else:
-        self.assertEqual(checksum.algorithm, doc[1])
-        self.assertEqual(checksum.value(), doc[2])
+        raise
+    else:
+      self.assertEqual(checksum.algorithm, doc[1])
+      self.assertEqual(checksum.value(), doc[2])
 
-    doctest(EG_CHECKSUM_GMN)
+  def test_checksum_serialization_0(self):
+
+    self.doctest(EG_CHECKSUM_GMN)
     # TODO.
-    #doctest(EG_CHECKSUM_KNB)
-    doctest(EG_BAD_CHECKSUM_1, shouldfail=True)
-    doctest(EG_BAD_CHECKSUM_2, shouldfail=True)
+    #self.doctest(EG_CHECKSUM_KNB)
+    self.doctest(EG_BAD_CHECKSUM_1, shouldfail=True)
+    self.doctest(EG_BAD_CHECKSUM_2, shouldfail=True)
+
+  def test_checksum_serialization_1(self):
+    '''Serialization: Checksum -> XML -> Checksum.
+    '''
+    f = checksum_serialization.Checksum('1' * 32)
+    f.checksum.algorithm = 'MD5'
+    xml_doc, content_type = f.serialize(d1_common.const.MIMETYPE_XML)
+    #<?xml version="1.0" ?><ns1:checksum algorithm="MD5" xmlns:ns1="http://dataone.org/service/types/0.5.1">11111111111111111111111111111111</ns1:checksum>
+    f.deserialize(xml_doc, d1_common.const.MIMETYPE_XML)
+
+  def test_checksum_serialization_2(self):
+    '''Serialization: Checksum -> JSON.
+    '''
+    f = checksum_serialization.Checksum('1' * 32)
+    f.checksum.algorithm = 'MD5'
+    json_doc, content_type = f.serialize(d1_common.const.MIMETYPE_JSON)
+    # {"checksum": "11111111111111111111111111111111", "algorithm": "MD5"}
+    f.deserialize(json_doc, d1_common.const.MIMETYPE_JSON)
+
+  def test_checksum_serialization_3(self):
+    '''Serialization: Checksum -> CSV.
+    '''
+    f = checksum_serialization.Checksum('1' * 32)
+    f.checksum.algorithm = 'MD5'
+    csv_doc, content_type = f.serialize(d1_common.const.MIMETYPE_CSV)
+    # 11111111111111111111111111111111,MD5
+    f.deserialize(csv_doc, d1_common.const.MIMETYPE_CSV)
 
 #===============================================================================
 if __name__ == "__main__":
