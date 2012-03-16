42758d9c709b9842242904c59e9be116958f0357
Andrew Pippin <pippin@nceas.ucsb.edu>
2012-03-16 22:06:36 +0000

    Handle non-ascii characters better.

--- d1_client_cli/src/d1_client_cli/print_level.py
+++ d1_client_cli/src/d1_client_cli/print_level.py
@@ -1,19 +1,22 @@
 def print_level(level, msg):
-  for l in str(msg).split('\n'):
-    print('{0: <8s}{1}'.format(level, l))
+  ''' Print the information in as Unicode safe manner as possible.
+  '''
+  for l in unicode(msg).split(u'\n'):
+    msg = u'%s%s' % (u'{0: <8s}'.format(level), unicode(l))
+    print msg.encode('utf-8')
 
 
 def print_debug(msg):
-  print_level('DEBUG', msg)
+  print_level(u'DEBUG', unicode(msg))
 
 
 def print_error(msg):
-  print_level('ERROR', msg)
+  print_level(u'ERROR', unicode(msg))
 
 
 def print_warn(msg):
-  print_level('WARN', msg)
+  print_level(u'WARN', unicode(msg))
 
 
 def print_info(msg):
-  print_level('', msg)
+  print_level(u'', unicode(msg))
