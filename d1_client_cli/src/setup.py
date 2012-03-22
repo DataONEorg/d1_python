495f9e17744cce3e783d50cb32dfd4bf7ab2a32c
Andrew Pippin <pippin@nceas.ucsb.edu>
2012-03-22 18:30:01 +0000

    Moving PyXB and Foresite requirements down to dependency_links since they are not available in PyPI. Adding entry point so the dataone module will be available as a binary.

--- d1_client_cli/src/setup.py
+++ d1_client_cli/src/setup.py
@@ -25,12 +25,19 @@ setup(
   install_requires=[
     'DataONE_Common >= 1.0.0c4',
     'DataONE_ClientLib >= 1.0.0c4',
-    'foresite >= 1.2',
-    'pyxb >= 1.1.3',
     'rdflib >= 2.4.2',
+    'python-dateutil',
+    'iso8601',
+  ],
+  dependency_links=[
+    'http://foresite-toolkit.googlecode.com/files/foresite-1.2.tgz',
+    'http://downloads.sourceforge.net/project/pyxb/pyxb/1.1.3/PyXB-full-1.1.3.tar.gz',
   ],
   package_data={
     # If any package contains *.txt or *.rst files, include them:
     '': ['*.txt', '*.rst'],
-  }
+  },
+  entry_points={
+    'console_scripts': ['dataone = d1_client_cli.dataone:main', ]
+  },
 )
