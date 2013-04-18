cffd86f763dc9e3b562d46cb10b265afef1d245e
Roger Dahl <git@dahlsys.com>
2013-04-18 14:53:30 +0000

    Completed example for parsing and displaying resource maps.

--- d1_libclient_python/src/examples/get_and_display_data_package.py
+++ d1_libclient_python/src/examples/get_and_display_data_package.py
@@ -60,24 +60,24 @@ import d1_client.mnclient
 # Config.
 
 # The identifier (PID) of the DataONE Data Package (Resource Map) to download
-# and display.
-SCIENCE_OBJECT_PID = 'dataone_test_object_pid'
+# and display. Data packages have Format ID
+# http://www.openarchives.org/ore/terms.
+SCIENCE_OBJECT_PID = 'dakoop_test-PKG'
 
-# BaseURL for the Member Node. If the script is run on the same server as the
-# Member Node, this can be localhost.
-MN_BASE_URL = 'https://gmn.test.dataone.org/mn'
+# BaseURL for the Member Node on which the science object resides. If the script
+# is run on the same server as the Member Node, this can be localhost.
+MN_BASE_URL = 'https://mn-demo-6.test.dataone.org/knb/d1/mn'
 #MN_BASE_URL = 'https://localhost/mn'
 
-# Paths to the certificate and key to use when creating the object. If the
-# certificate has the key embedded, the _KEY setting should be set to None. If
-# the objects have public access, these can both be set to None.
+# Paths to the certificate and key to use when retrieving the object. This is
+# required if the object is not publicly accessible. If the certificate has the
+# key embedded, the _KEY setting should be set to None. If the objects have
+# public access, these can both be set to None.
 CERTIFICATE_FOR_CREATE = None
 CERTIFICATE_FOR_CREATE_KEY = None
 
 
 def main():
-  assert False, "This example is not implemented yet."
-
   # Create a Member Node client that can be used for running commands against
   # a specific Member Node.
   client = d1_client.mnclient.MemberNodeClient(
@@ -86,8 +86,27 @@ def main():
     key_path=CERTIFICATE_FOR_CREATE_KEY
   )
 
+  # Use the client to get a data package as a string (Format ID
+  # http://www.openarchives.org/ore/terms).
+  r = client.get(SCIENCE_OBJECT_PID).read()
+
+  # Create a resource map parser.
   resource_map_parser = d1_client.data_package.ResourceMapParser()
-  print resource_map_parser.simple_parse_resource_map(r)
+
+  # Use the resource map parser to parse the resource map. Then display it.
+
+  print 'Triples:'
+
+  for s, p, o in resource_map_parser.get_triples_by_package(r):
+    print 'subject:   ', s
+    print 'predicate: ', p
+    print 'object:    ', o
+    print
+
+  print 'Identifiers referenced by package: '
+
+  for pid in resource_map_parser.get_identifiers_referenced_by_package(r):
+    print 'PID: ', pid
 
 
 if __name__ == '__main__':
