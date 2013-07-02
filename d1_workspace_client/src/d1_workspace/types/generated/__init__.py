92a6739c33dc3c633731b24db99d04c368deb497
vdave <dave.vieglais@gmail.com>
2013-07-02 17:43:45 +0000

    Added method to generate a textual description of a workspace folder, meant to be consumed by ONEDrive.

--- d1_workspace_client/src/d1_workspace/types/generated/__init__.py
+++ d1_workspace_client/src/d1_workspace/types/generated/__init__.py
@@ -18,3 +18,44 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from StringIO import StringIO
+
+def generateFolderHelpText(folder):
+  '''Generates a human readable description of the folder in rst format.
+  '''
+  res = StringIO()
+  t = u"Description of Folder %s" % folder.name
+  res.write(t + u"\n")
+  res.write(u"=" * len(t))
+  res.write(u"\n\n")
+  res.write(u"""The content present in workspace folders is determined by a list 
+of specific identifiers and by queries applied against the DataONE search index.
+""")
+  res.write("\n\n")
+  res.write(u"**Queries**\n")
+  res.write(u"\n")
+  if len(folder.query) > 0:
+    for query in folder.query:
+      res.write(u"* ``%s``\n" % query)
+  else:
+    res.write(u"No queries specified at this level.\n") 
+  res.write(u"\n\n")
+  res.write(u"**Identifiers**\n")
+  res.write(u"\n")
+  if len(folder.identifier) > 0:
+    for pid in folder.identifier:
+      res.write(u"* ``%s``\n" % pid)
+  else:
+    res.write(u"No individual identifiers selected at this level.\n") 
+  res.write(u"\n\n")
+  res.write(u"**Sub-folders**\n")
+  res.write(u"\n")
+  if len(folder.folder) > 0:
+    for f in folder.folder:
+      res.write(u"* ``%s``\n" % f.name)
+  else:
+    res.write(u"No workspace sub-folders are specified at this level.\n") 
+  text = res.getvalue()
+  return text.encode('utf8')
+
