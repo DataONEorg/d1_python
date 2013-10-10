55d3f6a8bf93ab132a31082036bf7d510bab9120
Roger Dahl <github@dahlsys.com>
2013-10-10 18:33:08 +0000

    - For now, I have to include a copy of the mimetype mappings from d1_libclient because py2exe doesn't automatically copy it. - Add build script for Inno Setup, used for building the Windows installer. - Added py2exe configuration to setup.py. Enables building the Windows executable for ONEDrive with "setup.py py2exe".

--- d1_client_onedrive/src/setup.py
+++ d1_client_onedrive/src/setup.py
@@ -27,11 +27,38 @@
 """
 
 from setuptools import setup, find_packages
-import impl
+import py2exe
+
+import d1_client_onedrive
+
+# http://www.py2exe.org/index.cgi/WorkingWithVariousPackagesAndModules
+# lxml
+# if missing _elementhpath, either pull whole lxml library in packages=..., or put "from lxml import _elementhpath as _dummy" somewhere in code; in both cases also pull gzip in packages=...
+
+opts = {
+  "py2exe": {
+    # if you import .py files from subfolders of your project, then those are
+    # submodules.  You'll want to declare those in the "includes"
+    #'includes':['d1_client_cli.impl.cli_util'],
+    'packages': [
+      'd1_client_onedrive',
+      'd1_client_onedrive.impl',
+      'd1_client_onedrive.impl.drivers',
+      'd1_client_onedrive.impl.drivers.dokan',
+      'd1_client_onedrive.impl.drivers.fuse',
+      'd1_client_onedrive.impl.resolver',
+      'rdflib.plugins',
+      'lxml'
+    ],
+    'skip_archive': True,
+  }
+}
+
 
 setup(
+  options = opts,
   name = 'dataone.onedrive',
-  version = impl.__version__,
+  version = d1_client_onedrive.__version__,
   description = 'Filesystem access to the DataONE Workspace',
   author = 'DataONE Project',
   author_email = 'developers@dataone.org',
@@ -47,12 +74,20 @@ setup(
   install_requires = [
     'dataone.common == 1.1.2RC1',
     'dataone.libclient == 1.2.1',
-    'fusepy',
+    #'fusepy',
   ],
 
   package_data = {
   },
 
   entry_points = {
+    'console_scripts': ['onedrive = d1_client_onedrive.onedrive:main',]
   },
+
+  # py2exe
+  console=['d1_client_onedrive/onedrive.py'],
+  data_files = [
+    ('d1_client', ['./mime_mappings.csv']),
+    ('', ['./workspace.xml']),
+  ]
 )
