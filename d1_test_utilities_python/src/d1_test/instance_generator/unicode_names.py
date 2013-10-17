bbc525ca61713db94a5b3bbf116a6d9dfbd5061b
Roger Dahl <github@dahlsys.com>
2013-10-17 22:34:36 +0000

    Moving d1_test sub-packages to their final locations.

---
+++ d1_test_utilities_python/src/d1_test/instance_generator/unicode_names.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This work was created by participants in the DataONE project, and is
+# jointly copyrighted by participating institutions in DataONE. For
+# more information on DataONE, see our web site at http://dataone.org.
+#
+#   Copyright 2011
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''
+Module d1_instance_generator.unicode_names
+==========================================
+
+:Synopsis: A few names in unicode pulled from
+  http://www.i18nguy.com/unicode-example.html.
+:Created: 2011-07-31
+:Author: DataONE (Vieglais, Dahl)
+'''
+
+UNICODE_NAMES = [
+  u"Արամ Խաչատրյան",
+  u"Johann Strauß",
+  u"Vaqif Səmədoğlu",
+  u"Вагиф Сәмәдоғлу",
+  u"Heydər Əliyev",
+  u"Һејдәр Әлијев",
+  u"René Magritte",
+  u"সুকুমার রায়",
+  u"མགོན་པོ་རྡོ་རྗེ།",
+  u"ព្រះ​ពុទ្ឋឃោសាចារ‌្យ​ជួន​ណាត",
+  u"Céline Dion",
+  u"ᓱᓴᓐ ᐊᒡᓗᒃᑲᖅ",
+  u"ᏍᏏᏉᏯ",
+  u"章子怡",
+  u"王菲",
+  u"Antonín Dvořák",
+  u"Søren Hauch-Fausbøll",
+  u"عبدالحليم حافظ",
+  u"أم كلثوم",
+  u"ብርሃነ ዘርኣይ",
+  u"ኃይሌ ገብረሥላሴ",
+  u"⠇⠕⠥⠊⠎⠀⠃⠗⠁⠊⠇⠇⠑",
+  u"ედუარდ შევარდნაძე",
+  u"Γιώργος Νταλάρας",
+  u"András Sütő",
+  u"Björk Guðmundsdóttir",
+  u"माधुरी दीक्षित",
+  u"محسن مخملباف",
+  u"Sinéad O’Connor",
+  u"יהורם גאון",
+  u"久保田    利伸",
+  u"林原 めぐみ",
+  u"宮崎　駿",
+  u"森鷗外",
+  u"テクス・テクサン",
+  u"이설희",
+  u"솅조ᇰ (세종대왕)",
+  u"Tor Åge Bringsværd",
+  u"نصرت فتح علی خان",
+  u"Pūblius Cornēlius Scīpiō Africānus",
+  u"Михаил Горбачёв",
+  u"Ἀρχιμήδης",
+  u"ธงไชย แม็คอินไตย์",
+]
