a5740edcee9783e94e29880264e709ec08e19ec9
Roger Dahl <github@dahlsys.com>
2013-12-20 02:53:26 +0000

    - Bumped version to 1.0.2.

--- d1_test_utilities_python/src/d1_test/__init__.py
+++ d1_test_utilities_python/src/d1_test/__init__.py
@@ -18,5 +18,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+'''d1_test_utilities_python'''
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
