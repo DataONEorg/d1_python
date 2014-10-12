dfc45121afeb2785755b419200f2febd74ff7cdc
Roger Dahl <git@dahlsys.com>
2014-10-12 16:06:15 +0000

    - Added missing file needed for unit tests.

---
+++ d1_libclient_python/src/d1_client/tests/settings.py
@@ -0,0 +1,28 @@
+import d1_common.const
+
+CN_URL = d1_common.const.URL_DATAONE_ROOT
+CN_HOST = 'cn.dataone.org'
+#self.baseurl = 'http://cn.dataone.org/cn'
+#self.baseurl = 'http://cn-dev.dataone.org/cn/'
+#self.baseurl = 'https://cn-dev.dataone.org/cn/'
+#self.baseurl = 'http://cn-dev-2.dataone.org/cn/'
+
+MN_URL = 'https://oneshare.unm.edu/knb/d1/mn'
+
+#self.baseurl = 'http://daacmn-dev.dataone.org/mn'
+#self.baseurl = 'http://localhost:8000/'
+#self.testpid = 'hdl:10255/dryad.105/mets.xml'
+#http://dev-dryad-mn.dataone.org/mn/meta/hdl:10255/dryad.105/mets.xml
+#http://dev-dryad-mn.dataone.org/mn/meta/hdl%3A10255%2Fdryad.105%2Fmets.xml
+#http://dev-dryad-mn.dataone.org/mn/meta/hdl:10255%2Fdryad.105%2Fmets.xml
+#http://dev-dryad-mn.dataone.org/mn/meta/hdl%3A10255/dryad.105/mets.xml
+#self.baseurl = 'https://dev-testing.dataone.org/testsvc/echomm'
+#cert_path = None
+
+# echo
+#self.baseurl = 'http://foobar3000.com'
+#self.baseurl = 'http://helloworld3000.com/'
+#self.baseurl = 'http://xformstest.org/cgi-bin/echoinstance.sh'
+#self.baseurl = 'http://tools.ietf.org'
+
+SOLR_QUERY_ENDPOINT = '/cn/v1/query/solr/'
