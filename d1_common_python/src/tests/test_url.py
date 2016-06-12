889d7f393769eb9ba38430b8e0f9e1434310855a
Roger Dahl <git@dahlsys.com>
2016-06-12 09:49:01 -0600

    Move testUnicodeStrings.utf8.txt to test folder
    This is the only file we were using from the test suite, so we just copy it
    here to avoid the dependency.
    
    Some cosmetic changes to better match PEP8.

--- d1_common_python/src/tests/test_url.py
+++ d1_common_python/src/tests/test_url.py
@@ -18,14 +18,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-'''
+"""
 Module d1_common.tests.test_url
 ===============================
 
 :Synopsis: Unit tests for url.py
 :Created: 2011-01-21
 :Author: DataONE (Vieglais, Dahl)
-'''
+"""
 
 # Stdlib.
 import codecs
@@ -35,17 +35,16 @@ import sys
 import unittest
 
 # D1.
-from d1_common import xmlrunner
 import d1_common.url
 
 # App
+HERE_DIR_PATH = os.path.abspath(os.path.dirname(__file__))
 
 
 class TestUrl(unittest.TestCase):
   def test_010(self):
-    '''encodePathElement()'''
-    fpath = os.path.abspath(os.path.dirname(__file__))
-    ftest = os.path.join(fpath, 'd1_testdocs/encodingTestSet/testUnicodeStrings.utf8.txt')
+    """encodePathElement()"""
+    ftest = os.path.join(HERE_DIR_PATH, 'testUnicodeStrings.utf8.txt')
     testfile = codecs.open(ftest, encoding='utf-8', mode='r')
     testrows = testfile.readlines()
     for row in testrows:
@@ -57,9 +56,9 @@ class TestUrl(unittest.TestCase):
           self.assertEqual(e, d1_common.url.encodePathElement(v))
 
   def test_020(self):
-    '''encodeQueryElement()'''
+    """encodeQueryElement()"""
     fpath = os.path.abspath(os.path.dirname(__file__))
-    ftest = os.path.join(fpath, 'd1_testdocs/encodingTestSet/testUnicodeStrings.utf8.txt')
+    ftest = os.path.join(fpath, 'testUnicodeStrings.utf8.txt')
     testfile = codecs.open(ftest, encoding='utf-8', mode='r')
     testrows = testfile.readlines()
     for row in testrows:
@@ -71,7 +70,7 @@ class TestUrl(unittest.TestCase):
           self.assertEqual(e, d1_common.url.encodeQueryElement(v))
 
   def test_030(self):
-    '''urlencode()'''
+    """urlencode()"""
     data = [
       ('a', '"#<>[]^`{}|'), ('b', '-&=&='), ('c', 'http://example.com/data/mydata?row=24')
     ]
@@ -80,7 +79,7 @@ class TestUrl(unittest.TestCase):
     self.assertEqual(test, expected)
 
   def test_040(self):
-    '''stripElementSlashes()'''
+    """stripElementSlashes()"""
     self.assertEqual('element', d1_common.url.stripElementSlashes('/element'))
     self.assertEqual('element', d1_common.url.stripElementSlashes('//element/'))
     self.assertEqual('element', d1_common.url.stripElementSlashes('element/'))
@@ -90,12 +89,12 @@ class TestUrl(unittest.TestCase):
     self.assertEqual('', d1_common.url.stripElementSlashes('//'))
 
   def test_050(self):
-    '''joinPathElements()'''
+    """joinPathElements()"""
     self.assertEqual('a/b', d1_common.url.joinPathElements('a', 'b'))
     self.assertEqual('a/b/c', d1_common.url.joinPathElements('a/', '/b', 'c'))
 
   def test_055(self):
-    '''joinPathElementsNoStrip()'''
+    """joinPathElementsNoStrip()"""
     self.assertEqual('', d1_common.url.joinPathElementsNoStrip(''))
     self.assertEqual('/', d1_common.url.joinPathElementsNoStrip('/'))
     self.assertEqual('//', d1_common.url.joinPathElementsNoStrip('//'))
@@ -113,7 +112,7 @@ class TestUrl(unittest.TestCase):
     )
 
   def test_060(self):
-    '''normalizeTarget()'''
+    """normalizeTarget()"""
     u0 = "http://some.server/base/mn/"
     u1 = "http://some.server/base/mn"
     u2 = "http://some.server/base/mn?"
@@ -124,7 +123,7 @@ class TestUrl(unittest.TestCase):
     self.assertEqual(u0, d1_common.url.normalizeTarget(u3))
 
   def test_070(self):
-    '''makeCNBaseURL()'''
+    """makeCNBaseURL()"""
     self.assertEqual(d1_common.url.makeCNBaseURL(''), 'https://cn.dataone.org/cn')
     self.assertEqual(d1_common.url.makeCNBaseURL('test.com'), 'https://test.com/cn')
     self.assertEqual(d1_common.url.makeCNBaseURL('test.com/cn'), 'https://test.com/cn')
@@ -147,7 +146,7 @@ class TestUrl(unittest.TestCase):
     )
 
   def test_080(self):
-    '''makeMNBaseURL()'''
+    """makeMNBaseURL()"""
     self.assertEqual(d1_common.url.makeMNBaseURL(''), 'https://localhost/mn')
     self.assertEqual(d1_common.url.makeMNBaseURL('test.com'), 'https://test.com/mn')
     self.assertEqual(d1_common.url.makeMNBaseURL('test.com/mn'), 'https://test.com/mn')
