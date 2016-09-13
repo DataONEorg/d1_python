691745f7c34d15010b349a9843f5e373d9dcdb3d
Roger Dahl <git@dahlsys.com>
2016-09-13 16:14:51 -0600

    PEP8 cleanup

--- d1_mn_generic/src/service/mn/auth.py
+++ d1_mn_generic/src/service/mn/auth.py
@@ -18,29 +18,27 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-''':mod:`auth`
+""":mod:`auth`
 ==============
 
 :Synopsis: Authentication and authorization.
 :Author: DataONE (Dahl)
-'''
+"""
 
 # Django.
 import django.core.cache
-import django.db
-import django.db.transaction
 from django.conf import settings
 
 # D1.
 import d1_common.const
 import d1_common.types.exceptions
-import d1_common.types.generated.dataoneTypes as dataoneTypes
-import d1_x509v3_certificate_extractor
-
+import d1_common.types.dataoneTypes
+import d1_certificate.certificate_extractor
+import d1_common.types.dataoneTypes
 # App.
 import mn.models
 import mn.node_registry
-import mn.sysmeta_store
+import mn.sysmeta_file
 
 # ------------------------------------------------------------------------------
 # Helpers.
@@ -61,13 +59,13 @@ WRITE_LEVEL = 1
 READ_STR = 'read'
 READ_LEVEL = 0
 
-action_level_map = {
+ACTION_LEVEL_MAP = {
   CHANGEPERMISSION_STR: CHANGEPERMISSION_LEVEL,
   WRITE_STR: WRITE_LEVEL,
   READ_STR: READ_LEVEL,
 }
 
-level_action_map = {
+LEVEL_ACTION_MAP = {
   CHANGEPERMISSION_LEVEL: CHANGEPERMISSION_STR,
   WRITE_LEVEL: WRITE_STR,
   READ_LEVEL: READ_STR,
@@ -75,24 +73,24 @@ level_action_map = {
 
 
 def action_to_level(action):
-  '''Map action name to action level.
-  '''
+  """Map action name to action level.
+  """
   try:
-    return action_level_map[action]
+    return ACTION_LEVEL_MAP[action]
   except LookupError:
     raise d1_common.types.exceptions.InvalidRequest(
-      0, 'Invalid action: {0}'.format(action)
+      0, u'Unknown action. action="{}"'.format(action)
     )
 
 
 def level_to_action(level):
-  '''Map action level to action name.
-  '''
+  """Map action level to action name.
+  """
   try:
-    return level_action_map[level]
+    return LEVEL_ACTION_MAP[level]
   except LookupError:
     raise d1_common.types.exceptions.InvalidRequest(
-      0, 'Invalid action level: {0}'.format(level)
+      0, u'Unknown action level. level="{}"'.format(level)
     )
 
 
@@ -130,22 +128,22 @@ def _get_client_side_certificate_pem():
     return open(settings.CLIENT_CERT_PATH, 'rb').read()
   except EnvironmentError as e:
     raise d1_common.types.exceptions.ServiceFailure(
-      0, 'Error reading client side certificate. File: {0}. Error: {1}'
+      0, u'Error reading client side certificate. cert_path="{}", error="{}"'
       .format(settings.CLIENT_CERT_PATH, str(e))
     )
 
 
 def _extract_subject_from_pem(cert_pem):
   try:
-    return d1_x509v3_certificate_extractor.extract(cert_pem)[0]
+    return d1_certificate.certificate_extractor.extract(cert_pem)[0]
   except Exception as e:
     raise d1_common.types.exceptions.InvalidToken(
-      0, 'Error extracting session from certificate: {0}'.format(str(e))
+      0, u'Could not extract session from certificate. error="{}"'.format(str(e))
     )
 
 
 def is_allowed(request, level, pid):
-  '''Check if one or more subjects are allowed to perform action on object.
+  """Check if one or more subjects are allowed to perform action on object.
   If a subject holds permissions for one action level on object, all lower
   action levels are also allowed. Any included subject that is unknown to this
   MN is treated as a subject without permissions.
@@ -153,7 +151,7 @@ def is_allowed(request, level, pid):
     True if one or more subjects are allowed to perform action on object.
     False if PID does not exist.
     False if level is invalid.
-  '''
+  """
   # If subjects contains one or more DataONE trusted infrastructure subjects,
   # all rights are given.
   if is_trusted_subject(request):
@@ -165,7 +163,7 @@ def is_allowed(request, level, pid):
   # - The permission must be for an action level that is the same or higher than
   # the requested action level.
   return mn.models.Permission.objects.filter(
-    object__pid=pid,
+    object__pid__sid_or_pid=pid,
     subject__subject__in=request.subjects,
     level__gte=level
   ).exists()
@@ -178,30 +176,32 @@ def has_create_update_delete_permission(request):
 
 
 def assert_create_update_delete_permission(request):
-  '''Access only by subjects with Create/Update/Delete permission and by
+  """Access only by subjects with Create/Update/Delete permission and by
   trusted infrastructure (CNs).
-  '''
+  """
   if not has_create_update_delete_permission(request):
     raise d1_common.types.exceptions.NotAuthorized(
-      0, 'Access allowed only for subjects with Create/Update/Delete '
-      'permission. {0}'.format(format_active_subjects(request))
+      0, u'Access allowed only for subjects with Create/Update/Delete '
+      u'permission. active_subjects="{}"'.format(format_active_subjects(request))
     )
 
 
 def assert_allowed(request, level, pid):
-  '''Assert that one or more subjects are allowed to perform action on object.
+  """Assert that one or more subjects are allowed to perform action on object.
   Raise NotAuthorized if object exists and subject is not allowed.
   Raise NotFound if object does not exist.
   Return NoneType if subject is allowed.
-  '''
-  if not mn.models.ScienceObject.objects.filter(pid=pid).exists():
+  """
+  if not mn.models.ScienceObject.objects.filter(pid__sid_or_pid=pid).exists():
     raise d1_common.types.exceptions.NotFound(
-      0, 'Attempted to perform operation on non-existing object', pid
+      0,
+      u'Attempted to perform operation on non-existing object. pid="{}"'.format(pid)
     )
   if not is_allowed(request, level, pid):
     raise d1_common.types.exceptions.NotAuthorized(
-      0, u'{0} on "{1}" denied. {2}'.format(
-        level_to_action(level), pid, format_active_subjects(request)
+      0,
+      u'Operation is denied. level="{}", pid="{}", active_subjects="{}"'
+        .format(level_to_action(level), pid, format_active_subjects(request)
       )
     )
 
@@ -219,41 +219,35 @@ def assert_allowed(request, level, pid):
 
 
 def assert_trusted_permission(f):
-  '''Access only by D1 infrastructure.
-  '''
-
+  """Access only by D1 infrastructure.
+  """
   def wrap(request, *args, **kwargs):
     assert_trusted(request)
     return f(request, *args, **kwargs)
-
   wrap.__doc__ = f.__doc__
   wrap.__name__ = f.__name__
   return wrap
 
 
 def assert_list_objects_access(f):
-  '''Access to listObjects() controlled by settings.PUBLIC_OBJECT_LIST.
-  '''
-
+  """Access to listObjects() controlled by settings.PUBLIC_OBJECT_LIST.
+  """
   def wrap(request, *args, **kwargs):
     if not settings.PUBLIC_OBJECT_LIST:
       assert_trusted(request)
     return f(request, *args, **kwargs)
-
   wrap.__doc__ = f.__doc__
   wrap.__name__ = f.__name__
   return wrap
 
 
 def assert_get_log_records_access(f):
-  '''Access to getLogRecords() controlled by settings.PUBLIC_LOG_RECORDS.
-  '''
-
+  """Access to getLogRecords() controlled by settings.PUBLIC_LOG_RECORDS.
+  """
   def wrap(request, *args, **kwargs):
     if not settings.PUBLIC_LOG_RECORDS:
       assert_trusted(request)
     return f(request, *args, **kwargs)
-
   wrap.__doc__ = f.__doc__
   wrap.__name__ = f.__name__
   return wrap
@@ -262,107 +256,100 @@ def assert_get_log_records_access(f):
 def assert_trusted(request):
   if not is_trusted_subject(request):
     raise d1_common.types.exceptions.NotAuthorized(
-      0, 'Access allowed only for DataONE infrastructure. {0}. '
-      'Trusted subjects: {1}'.format(
+      0,
+      u'Access allowed only for trusted subjects. active_subjects="{}", '
+      u'trusted_subjects="{}"'.format(
         format_active_subjects(request), get_trusted_subjects_string()
       )
     )
 
 
 def decorator_assert_create_update_delete_permission(f):
-  '''Access only by subjects with Create/Update/Delete permission and by
+  """Access only by subjects with Create/Update/Delete permission and by
   trusted infrastructure (CNs).
-  '''
-
+  """
   def wrap(request, *args, **kwargs):
     assert_create_update_delete_permission(request)
     return f(request, *args, **kwargs)
-
   wrap.__doc__ = f.__doc__
   wrap.__name__ = f.__name__
   return wrap
 
-
 def assert_authenticated(f):
-  '''Access only with a valid session.
-  '''
-
+  """Access only with a valid session.
+  """
   def wrap(request, *args, **kwargs):
     if d1_common.const.SUBJECT_AUTHENTICATED not in request.subjects:
       raise d1_common.types.exceptions.NotAuthorized(
-        0, 'Access allowed only for authenticated subjects. Please reconnect with '
-        'a valid DataONE session certificate. {0}'.format(
+        0, u'Access allowed only for authenticated subjects. Please reconnect with '
+        u'a valid DataONE session certificate. active_subjects="{}"'.format(
           format_active_subjects(
             request
           )
         )
       )
     return f(request, *args, **kwargs)
-
   wrap.__doc__ = f.__doc__
   wrap.__name__ = f.__name__
   return wrap
 
 
 def assert_verified(f):
-  '''Access only with a valid session where the primary subject is verified.
-  '''
-
+  """Access only with a valid session where the primary subject is verified.
+  """
   def wrap(request, *args, **kwargs):
     if d1_common.const.SUBJECT_VERIFIED not in request.subjects:
       raise d1_common.types.exceptions.NotAuthorized(
-        0, 'Access allowed only for verified accounts. Please reconnect with a '
-        'valid DataONE session certificate in which the identity of the '
-        'primary subject has been verified. {0}'.format(format_active_subjects(request))
+        0, u'Access allowed only for verified accounts. Please reconnect with a '
+        u'valid DataONE session certificate in which the identity of the '
+        u'primary subject has been verified. active_subjects="{}"'
+          .format(format_active_subjects(request))
       )
     return f(request, *args, **kwargs)
-
   wrap.__doc__ = f.__doc__
   wrap.__name__ = f.__name__
   return wrap
 
 
 def assert_required_permission(f, level):
-  '''Assert that subject has access at given level or higher for object.
-  '''
-
+  """Assert that subject has access at given level or higher for object.
+  """
   def wrap(request, pid, *args, **kwargs):
     assert_allowed(request, level, pid)
     return f(request, pid, *args, **kwargs)
-
   wrap.__doc__ = f.__doc__
   wrap.__name__ = f.__name__
   return wrap
 
 
 def assert_changepermission_permission(f):
-  '''Assert that subject has changePermission or high for object.
-  '''
+  """Assert that subject has changePermission or high for object.
+  """
   return assert_required_permission(f, CHANGEPERMISSION_LEVEL)
 
 
 def assert_write_permission(f):
-  '''Assert that subject has write permission or higher for object.
-  '''
+  """Assert that subject has write permission or higher for object.
+  """
   return assert_required_permission(f, WRITE_LEVEL)
 
 
 def assert_read_permission(f):
-  '''Assert that subject has read permission or higher for object.
-  '''
+  """Assert that subject has read permission or higher for object.
+  """
   return assert_required_permission(f, READ_LEVEL)
 
 
 def format_active_subjects(request):
-  '''Create a string listing active subjects for this connection, suitable
+  """Create a string listing active subjects for this connection, suitable
   for appending to authentication error messages.
-  '''
+  """
   decorated_subjects = []
   for subject in request.subjects:
     if subject == request.primary_subject:
-      decorated_subjects.append(subject + ' (primary)')
+      decorated_subjects.append(subject + u' (primary)')
     elif subject == d1_common.const.SUBJECT_VERIFIED:
-      decorated_subjects.append(subject + ' (verified)')
+      decorated_subjects.append(subject + u' (verified)')
     else:
-      decorated_subjects.append(subject + ' (equivalent)')
-  return 'Active subjects: {0}'.format(', '.join(decorated_subjects))
+      decorated_subjects.append(subject + u' (equivalent)')
+  return u'Active subjects: {}'.format(u', '.join(decorated_subjects))
