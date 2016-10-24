e5ad7bc0429d4085bc7ba331deffecf30289bb90
Roger Dahl <git@dahlsys.com>
2016-10-24 08:58:52 -0600

    Rewrite replication to use new model structure for replicas
    The new model structure keeps track of more information about replicas,
    related to obsolescence chains, SIDs and space requirements.

--- d1_mn_generic/src/service/mn/management/commands/process_replication_queue.py
+++ d1_mn_generic/src/service/mn/management/commands/process_replication_queue.py
@@ -81,78 +81,114 @@ class ReplicationQueueProcessor(object):
     self.cn_client = self._create_cn_client()
 
   def process_replication_queue(self):
-    q = mn.models.ReplicationQueue.objects.exclude(status__status='completed')
-    if not len(q):
-      self.logger.debug('No replication requests to process')
+    queue_queryset = mn.models.ReplicationQueue.objects.filter(
+      local_replica__info__status__status='queued'
+    )
+    if not len(queue_queryset):
+      logging.debug(u'No replication requests to process')
       return
-    for task in q:
-      self._process_replication_task(task)
-    self._remove_completed_tasks_from_queue()
+    for queue_model in queue_queryset:
+      self._process_replication_request(queue_model)
+    self._remove_completed_requests_from_queue()
 
-  def _process_replication_task(self, task):
-    self.logger.info('-' * 79)
-    self.logger.info('Processing PID: {}'.format(task.pid))
+  def _process_replication_request(self, queue_model):
+    logging.info(u'-' * 79)
+    logging.info(
+      u'Processing PID: {}'.format(queue_model.local_replica.pid.did)
+    )
     try:
-      self._replicate(task)
-    except d1_common.types.exceptions.DataONEException as e:
-      self.logger.exception(u'Replication failed with DataONE Exception:')
-      self._cn_replicate_task_update(task, 'failed', e)
-      self._gmn_replicate_task_update(task, str(e))
-    except (ReplicateError, Exception, object) as e:
-      self.logger.exception(u'Replication failed with internal exception:')
-      self._cn_replicate_task_update(task, 'failed')
-      self._gmn_replicate_task_update(task, str(e))
-    return True
-
-  def _replicate(self, task):
-    sys_meta = self._get_system_metadata(task)
-    sci_obj_stream = self._get_sci_obj_stream(task)
-    self._create_replica(sys_meta, sci_obj_stream)
-    self._gmn_replicate_task_update(task, 'completed')
-    self._cn_replicate_task_update(task, 'completed')
-
-  def _cn_replicate_task_update(self, task, replication_status, e=None):
-    try:
-      self.cn_client.setReplicationStatus(
-        task.pid, settings.NODE_IDENTIFIER, replication_status, e
-      )
+      self._replicate(queue_model)
     except Exception as e:
-      self.logger.exception(
-        u'CNReplication.setReplicationStatus() failed with '
-        u'the following exception:'
-      )
+      logging.exception(u'Replication failed with exception:')
+      num_attempts = self._inc_and_get_replication_attempts(queue_model)
+      if num_attempts < settings.REPLICATION_MAX_ATTEMPTS:
+        logging.warning(
+          u'Replication failed and will be retried during next processing. '
+          u'failed_attempts={}, max_attempts={}'.
+          format(num_attempts, settings.REPLICATION_MAX_ATTEMPTS)
+        )
+      else:
+        logging.warning(
+          u'Replication failed and has reached the maximum number of attempts. '
+          u'Recording the request as permanently failed and notifying the CN. '
+          u'failed_attempts={}, max_attempts={}'.
+          format(num_attempts, settings.REPLICATION_MAX_ATTEMPTS)
+        )
+        self._update_request_status(
+          queue_model, 'failed', e if
+          isinstance(e, d1_common.types.exceptions.DataONEException) else None
+        )
 
-  def _gmn_replicate_task_update(self, task, status=None):
-    if status is None or status == '':
-      status = 'Unknown error. See replication log.'
-    task.status = mn.models.replication_queue_status(status)
-    task.save()
+  def _replicate(self, queue_model):
+    with django.db.transaction.atomic():
+      sysmeta_obj = self._get_system_metadata(queue_model)
+      self._set_origin(queue_model, sysmeta_obj)
+      sciobj_stream = self._get_sciobj_stream(queue_model)
+      self._create_replica(sysmeta_obj, sciobj_stream)
+      self._update_request_status(queue_model, 'completed')
 
-  def _remove_completed_tasks_from_queue(self):
-    q = mn.models.ReplicationQueue.objects.filter(status__status='completed')
-    q.delete()
+  def _set_origin(self, queue_model, sysmeta_obj):
+    if sysmeta_obj.originMemberNode is None:
+      sysmeta_obj.originMemberNode = \
+        queue_model.local_replica.info.member_node.urn
+    if sysmeta_obj.authoritativeMemberNode is None:
+      sysmeta_obj.authoritativeMemberNode = \
+        queue_model.local_replica.info.member_node.urn
+    if sysmeta_obj.serialVersion is None:
+      sysmeta_obj.serialVersion = 1
+
+  def _inc_and_get_replication_attempts(self, queue_model):
+    replication_queue_model = mn.models.ReplicationQueue.objects.get(
+      local_replica=queue_model.local_replica
+    )
+    replication_queue_model.replication_attempts += 1
+    replication_queue_model.save()
+    return replication_queue_model.replication_attempts
+
+  def _update_request_status(self, queue_model, status_str, dataone_error=None):
+    self._update_local_request_status(queue_model, status_str)
+    self._update_cn_request_status(queue_model, status_str, dataone_error)
+
+  def _update_local_request_status(self, queue_model, status_str):
+    mn.models.update_replica_status(queue_model.local_replica.info, status_str)
+
+  def _update_cn_request_status(
+    self, queue_model, status_str, dataone_error=None
+  ):
+    self.cn_client.setReplicationStatus(
+      queue_model.local_replica.pid.did, settings.NODE_IDENTIFIER, status_str,
+      dataone_error
+    )
+
+  def _remove_completed_requests_from_queue(self):
+    mn.models.ReplicationQueue.objects.filter(
+      local_replica__info__status__status='completed'
+    ).delete()
 
   def _create_cn_client(self):
     return d1_client.cnclient.CoordinatingNodeClient(
       base_url=settings.DATAONE_ROOT,
       cert_path=settings.CLIENT_CERT_PATH,
-      key_path=settings.CLIENT_CERT_PRIVATE_KEY_PATH
+      key_path=settings.CLIENT_CERT_PRIVATE_KEY_PATH,
     )
 
-  def _get_system_metadata(self, task):
-    self.logger.debug(u'Calling CNRead.getSystemMetadata(pid={})'.format(task.pid))
-    return self.cn_client.getSystemMetadata(task.pid)
+  def _get_system_metadata(self, queue_model):
+    pid = queue_model.local_replica.pid.did
+    logging.debug(u'Calling CNRead.getSystemMetadata() pid={}'.format(pid))
+    return self.cn_client.getSystemMetadata(pid)
 
-  def _get_sci_obj_stream(self, task):
+  def _get_sciobj_stream(self, queue_model):
     source_node_base_url = self._resolve_source_node_id_to_base_url(
-      task.source_node.source_node
+      queue_model.local_replica.info.member_node.urn
     )
     mn_client = d1_client.mnclient.MemberNodeClient(
       base_url=source_node_base_url,
       cert_path=settings.CLIENT_CERT_PATH,
-      key_path=settings.CLIENT_CERT_PRIVATE_KEY_PATH
+      key_path=settings.CLIENT_CERT_PRIVATE_KEY_PATH,
     )
-    return self._open_sci_obj_stream_on_member_node(mn_client, task.pid)
+    return self._open_sciobj_stream_on_member_node(
+      mn_client, queue_model.local_replica.pid.did
+    )
 
   def _resolve_source_node_id_to_base_url(self, source_node):
     node_list = self._get_node_list()
@@ -165,92 +201,64 @@ class ReplicationQueueProcessor(object):
     raise ReplicateError(
       u'Unable to resolve Source Node ID. '
       u'source_node="{}", discovered_nodes="{}"'
-        .format(source_node, u', '.join(discovered_nodes))
+      .format(source_node, u', '.join(discovered_nodes))
     )
 
   def _get_node_list(self):
     return self.cn_client.listNodes()
 
-  def _open_sci_obj_stream_on_member_node(self, mn_client, pid):
+  def _open_sciobj_stream_on_member_node(self, mn_client, pid):
     return mn_client.getReplica(pid)
 
-  def _create_replica(self, sys_meta, sci_obj):
-    with transaction.atomic():
-      pid = sys_meta.identifier.value()
-      self._assert_pid_does_not_exist(pid)
-      self._store_sys_meta(pid, sys_meta)
-      self._store_science_object_bytes(pid, sci_obj)
-      sci_obj_row = self._create_database_entry_for_science_object(pid, sci_obj, sys_meta)
-      self._create_database_entry_for_object_create_event(sci_obj_row)
-      self._set_sys_meta_access_policy(pid, sys_meta)
+  def _create_replica(self, sysmeta_obj, sciobj_stream):
+    """GMN handles replicas differently from native objects, with the main
+    differences being related to handling of restrictions related to
+    obsolescence chains and SIDs. So this create sequence differs significantly
+    from the regular one that is accessed through MNStorage.create().
+    """
+    pid = sysmeta_obj.identifier.value()
+    self._assert_is_pid_of_local_unprocessed_replica(pid)
+    self._check_and_create_replica_obsolescence(sysmeta_obj, 'obsoletes')
+    self._check_and_create_replica_obsolescence(sysmeta_obj, 'obsoletedBy')
+    url = u'file:///{}'.format(d1_common.url.encodePathElement(pid))
+    sciobj_model = mn.sysmeta.create(sysmeta_obj, url)
+    self._store_science_object_bytes(pid, sciobj_stream)
+    mn.event_log.create_log_entry(
+      sciobj_model, 'create', '0.0.0.0', '[replica]', '[replica]'
+    )
 
-  def _store_sys_meta(self, pid, sys_meta):
-    if not sys_meta.serialVersion:
-      sys_meta.serialVersion = 0
-    mn.sysmeta_file.write_sysmeta_to_file(sys_meta)
+  def _check_and_create_replica_obsolescence(self, sysmeta_obj, attr_str):
+    obsolescence_attr = getattr(sysmeta_obj, attr_str)
+    if obsolescence_attr is not None:
+      pid = obsolescence_attr.value()
+      self._assert_pid_is_unknown_or_replica(pid)
+      self._create_replica_obsolescence_reference(pid)
 
-  def _store_science_object_bytes(self, pid, sci_obj):
-    object_path = mn.util.store_path(settings.OBJECT_STORE_PATH, pid)
+  def _create_replica_obsolescence_reference(self, pid):
+    mn.models.replica_obsolescence_chain_reference(pid)
+
+  def _store_science_object_bytes(self, pid, sciobj_stream):
+    object_path = mn.util.file_path(settings.OBJECT_STORE_PATH, pid)
     mn.util.create_missing_directories(object_path)
     with open(object_path, 'wb') as f:
-      shutil.copyfileobj(sci_obj, f)
+      shutil.copyfileobj(sciobj_stream, f)
 
-  def _assert_pid_does_not_exist(self, pid):
-    if mn.models.ScienceObject.objects.filter(pid__sid_or_pid=pid).exists():
+  def _assert_is_pid_of_local_unprocessed_replica(self, pid):
+    if not mn.sysmeta_replica.is_unprocessed_local_replica(pid):
       raise ReplicateError(
-        u'Another object with the identifier has already been created. GMN '
-        u'attempts to prevent this from happening by rejecting regular '
-        u'MNStorage.create() for objects with pids for which there are accepted '
-        u'replication requests that have not yet been processed.', pid
+        u'The identifier is already in use on the local Member Node. '
+        u'pid="{}"'.format(pid)
       )
-    # TODO: Identifiers are now factored out to a separate table. By using the
-    # same table for replication requests, I can now ensure that this does not
-    # happen, at the database level.
 
-  def _create_database_entry_for_science_object(self, pid, sci_obj, sys_meta):
-    id_obj = mn.models.IdNamespace()
-    id_obj.sid_or_pid = sys_meta.identifier.value()
-    id_obj.save()
-
-    sci_obj = mn.models.ScienceObject()
-    sci_obj.pid = id_obj
-    sci_obj.url = u'file:///{}'.format(d1_common.url.encodePathElement(pid))
-    sci_obj.set_format(sys_meta.formatId)
-    sci_obj.checksum = sys_meta.checksum.value()
-    sci_obj.checksum_algorithm = mn.models.checksum_algorithm(sys_meta.checksum.algorithm)
-    sci_obj.modified_timestamp = sys_meta.dateSysMetadataModified
-    sci_obj.size = sys_meta.size
-    sci_obj.replica = True
-    sci_obj.serial_version = sys_meta.serialVersion
-    sci_obj.is_archived = False
-    sci_obj.save()
-    return sci_obj
-
-  def _create_database_entry_for_object_create_event(self, sci_obj_row):
-    event_log_row = mn.models.EventLog()
-    event_log_row.sciobj = sci_obj_row
-    event_log_row.event = mn.models.event('create')
-    event_log_row.set_ip_address('[replica]')
-    event_log_row.set_user_agent('[replica]')
-    event_log_row.subject = mn.models.subject('[replica]')
-    event_log_row.save()
-
-  def _set_sys_meta_access_policy(self, pid, sys_meta):
-    if sys_meta.accessPolicy:
-      mn.auth.set_access_policy(pid, sys_meta.accessPolicy)
-    else:
-      mn.auth.set_access_policy(pid)
+  def _assert_pid_is_unknown_or_replica(self, pid):
+    if mn.sysmeta.is_did(pid) and not mn.sysmeta_replica.is_local_replica(pid):
+      raise ReplicateError(
+        u'The identifier is already in use on the local Member Node. '
+        u'pid="{}"'.format(pid)
+      )
 
 # ==============================================================================
 
 
 class ReplicateError(Exception):
-  def __init__(self, error_msg, pid=None):
-    self.error_msg = error_msg
-    self.pid = pid
-
-  def __str__(self):
-    msg = str(self.error_msg)
-    if self.pid is not None:
-      msg += u'\nIdentifier: {}'.format(self.pid)
-    return msg
+  pass
