932aeaaf6a730a7e2ec6b2ca1aa29b34703b3ae6
Roger Dahl <git@dahlsys.com>
2016-10-24 21:01:45 -0600

    Handle possible conflict between certificate and JWT
    If both certificate and JWT are provided and contain different primary
    subjects, make sure both are preserved.

--- d1_mn_generic/src/service/mn/middleware/view_handler.py
+++ d1_mn_generic/src/service/mn/middleware/view_handler.py
@@ -17,27 +17,30 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """View handler middleware
 """
 
 # Stdlib.
-import logging
 import StringIO
+import d1_common
+import logging
 
 # Django.
 from django.conf import settings
 
 # App.
-import session
+import session_cert
+import session_jwt
 
 
 class ViewHandler(object):
   def process_view(self, request, view_func, view_args, view_kwargs):
     # Log which view is about the be called.
     logging.info(
-      u'View: func_name={}, method={}, args={}, kwargs={}'
-      .format(view_func.func_name, request.method, view_args, view_kwargs)
+      u'View: func_name="{}", method="{}", args="{}", kwargs="{}", url="{}"'
+      .format(view_func.func_name, request.method, view_args, view_kwargs,
+        request.path_info
+      )
     )
     # logging.debug(request.headers)
     self.process_session(request)
@@ -59,8 +62,29 @@ class ViewHandler(object):
           self.pem_in_http_header_to_pem_in_string(
             request.META['HTTP_VENDOR_INCLUDE_CERTIFICATE'])
 
-    # Always run regular certificate processing.
-    session.ProcessSession(request)
+    # Add subjects from any provided certificate and JWT and store them in
+    # the Django request obj.
+    cert_primary_str, cert_equivalent_set = session_cert.get_subjects(request)
+    jwt_subject_list = session_jwt.validate_jwt_and_get_subject_list(request)
+    primary_subject_str = cert_primary_str
+    all_subjects_set = cert_equivalent_set | {cert_primary_str} | set(jwt_subject_list)
+    if len(jwt_subject_list) == 1:
+      jwt_primary_str = jwt_subject_list[0]
+      if jwt_primary_str != cert_primary_str:
+        if cert_primary_str == d1_common.const.SUBJECT_PUBLIC:
+          primary_subject_str = jwt_primary_str
+        else:
+          logging.warning(
+            'Both a certificate and a JWT were provided and the primary '
+            'subjects differ. Using the certificate for primary subject and'
+            'the JWT as equivalent.'
+          )
+    logging.info(u'Primary subject: {}'.format(primary_subject_str))
+    logging.info(u'All subjects: {}'.format(
+      ', '.join(sorted(all_subjects_set)))
+    )
+    request.primary_subject_str = primary_subject_str
+    request.all_subjects_set = all_subjects_set
 
     # Handle list of subjects in vendor specific extension:
     if settings.DEBUG_GMN:
@@ -68,7 +92,9 @@ class ViewHandler(object):
       # from any included client side certificate. The public symbolic principal
       # is always included in the subject list.
       if 'HTTP_VENDOR_INCLUDE_SUBJECTS' in request.META:
-        request.subjects.update(request.META['HTTP_VENDOR_INCLUDE_SUBJECTS'].split('\t'))
+        request.all_subjects_set.update(
+          request.META['HTTP_VENDOR_INCLUDE_SUBJECTS'].split('\t')
+        )
 
   def pem_in_http_header_to_pem_in_string(self, header_str):
     header = StringIO.StringIO(header_str)
