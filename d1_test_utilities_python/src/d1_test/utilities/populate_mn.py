d581cd178a8788a49c4560127a656b0acd83c8cd
Roger Dahl <git@dahlsys.com>
2016-10-25 00:34:53 -0600

    Add utility to populate MN with randomly generated objects

---
+++ d1_test_utilities_python/src/d1_test/utilities/populate_mn.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This work was created by participants in the DataONE project, and is
+# jointly copyrighted by participating institutions in DataONE. For
+# more information on DataONE, see our web site at http://dataone.org.
+#
+#   Copyright 2009-2016 DataONE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Populate a Member Node with randomly generated objects
+"""
+
+# Stdlib.
+import codecs
+import datetime
+import hashlib
+import logging
+import optparse
+import os
+import sys
+import StringIO
+
+# D1.
+import d1_client.mnclient
+import d1_client.mnclient_2_0
+import d1_common.types.exceptions
+
+# App
+import test_object_generator
+
+# Defaults
+MN_BASE_URL = 'https://localhost/mn'
+CERT_PUB_PEM_PATH = None
+CERT_KEY_PEM_PATH = None
+TIMEOUT_SEC = 60
+NUM_OBJECTS = 1000
+NUM_MIN_BYTES = 1024 * 10
+NUM_MAX_BYTES = 1024 * 100
+
+
+def main():
+  logging.basicConfig()
+  logging.getLogger('').setLevel(logging.DEBUG)
+
+  parser = optparse.OptionParser()
+
+  parser.add_option(
+    '--mn-base-url',
+    dest='mn_base_url',
+    action='store',
+    type='string',
+    default=MN_BASE_URL,
+    help='the base URL for the Member Node to populate',
+  )
+  parser.add_option(
+    '--cert-pub',
+    dest='cert_pub_path',
+    action='store',
+    type='string',
+    default=CERT_PUB_PEM_PATH,
+    help='path to PEM formatted public key of certificate',
+  )
+  parser.add_option(
+    '--cert-key',
+    dest='cert_key_path',
+    action='store',
+    type='string',
+    default=CERT_KEY_PEM_PATH,
+    help='path to PEM formatted private key of certificate',
+  )
+  parser.add_option(
+    '--timeout',
+    dest='timeout',
+    action='store',
+    type='float',
+    default=TIMEOUT_SEC,
+    help='amount of time to wait for calls to complete (seconds)',
+  )
+  parser.add_option(
+    '--num-objects',
+    dest='num_objects',
+    action='store',
+    type='int',
+    default=NUM_OBJECTS,
+    help='number of objects to create',
+  )
+  parser.add_option(
+    '--num-min-bytes',
+    dest='num_min_bytes',
+    action='store',
+    type='int',
+    default=NUM_MIN_BYTES,
+    help='minimum number of bytes in each object',
+  )
+  parser.add_option(
+    '--num-max-bytes',
+    dest='num_max_bytes',
+    action='store',
+    type='int',
+    default=NUM_MAX_BYTES,
+    help='maximum number of bytes in each object',
+  )
+  parser.add_option(
+    '--disable-tls-validate',
+    dest='disable_tls_validate',
+    action='store_true',
+    default=False,
+    help='disable validation of server side certificate',
+  )
+  parser.add_option(
+    '--use-v1',
+    action='store_true',
+    default=False,
+    dest='use_v1',
+    help='use the v1 API (v2 is default)',
+  )
+  parser.add_option(
+    '--debug',
+    action='store_true',
+    default=False,
+    dest='debug',
+    help='debug level logging',
+  )
+
+  (options, args) = parser.parse_args()
+
+  logging.getLogger('').setLevel(logging.DEBUG if options.debug else logging.INFO)
+
+  if options.use_v1:
+    mn_client = d1_client.mnclient.MemberNodeClient(
+      options.mn_base_url,
+      cert_path=options.cert_pub_path,
+      key_path=options.cert_key_path,
+    )
+  else:
+    mn_client = d1_client.mnclient_2_0.MemberNodeClient_2_0(
+      options.mn_base_url,
+      cert_path=options.cert_pub_path,
+      key_path=options.cert_key_path,
+    )
+
+  for _ in range(options.num_objects):
+    pid = test_object_generator.generate_random_ascii('pid')
+    sysmeta_pyxb, sciobj_str = test_object_generator \
+      .generate_science_object_with_sysmeta(
+        pid, options.num_min_bytes, options.num_max_bytes,
+        use_v1_bool=options.use_v1,
+    )
+    try:
+      mn_client.create(pid, StringIO.StringIO(sciobj_str), sysmeta_pyxb)
+    except d1_common.types.exceptions.DataONEException as e:
+      logging.exception('MNStorage.create() failed with exception:')
+      if len(e.traceInformation) >= 100:
+        trace_path = 'traceInformation.out'
+        with open(trace_path, 'wb') as f:
+          f.write(e.traceInformation)
+          logging.error('Dumped traceInformation to file: {}'.format(trace_path))
+          sys.exit()
+
+
+if __name__ == '__main__':
+  main()
---
+++ d1_test_utilities_python/src/d1_test/utilities/test_object_generator.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+
+# This work was created by participants in the DataONE project, and is
+# jointly copyrighted by participating institutions in DataONE. For
+# more information on DataONE, see our web site at http://dataone.org.
+#
+#   Copyright 2009-2016 DataONE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Stdlib
+import datetime
+import hashlib
+import logging
+import random
+import string
+
+# D1
+import d1_test.instance_generator
+import d1_test.instance_generator.random_data
+import d1_common.checksum
+import d1_common.const
+import d1_common.types.dataoneTypes_v1 as v1
+import d1_common.types.dataoneTypes_v2_0 as v2
+
+
+# Defaults
+FORMAT_ID = 'application/octet-stream'
+
+
+def generate_random_ascii(prefix, num_chars=10):
+  return '{0}_{1}'.format(
+    prefix, ''.join(
+      random.
+      choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)
+      for _ in range(num_chars)
+    )
+  )
+
+
+def generate_science_object_with_sysmeta(
+  pid,
+  num_min_bytes,
+  num_max_bytes,
+  format_id=FORMAT_ID,
+  include_obsolescence_bool=False,
+  use_v1_bool=False,
+):
+  sci_obj = _create_science_object_bytes(pid, num_min_bytes, num_max_bytes)
+  sys_meta = _generate_system_metadata_for_science_object(
+    pid, sci_obj, format_id, include_obsolescence_bool, use_v1_bool
+  )
+  return sys_meta, sci_obj
+
+
+def _create_science_object_bytes(pid, num_min_bytes, num_max_bytes):
+  """Create a string of pseudo-random bytes that are always the same for a given
+  {pid}. The length if set randomly between {num_min_bytes} and {num_max_bytes}
+  including.
+  """
+  # Seeding the PRNG with the PID causes the same sequence to be generated each
+  # time.
+  random.seed(pid)
+  num_bytes = random.randint(num_min_bytes, num_max_bytes)
+  return d1_test.instance_generator.random_data.random_bytes(num_bytes)
+
+
+def _generate_system_metadata_for_science_object(
+  pid, sciobj_str, format_id, include_obsolescence_bool, use_v1_bool
+):
+  now = datetime.datetime.now()
+  if use_v1_bool:
+    binding = v1
+  else:
+    binding = v2
+
+  sysmeta_pyxb = binding.systemMetadata()
+  sysmeta_pyxb.accessPolicy = _generate_public_access_policy(binding)
+  sysmeta_pyxb.checksum = d1_common.checksum.create_checksum_object(sciobj_str)
+  sysmeta_pyxb.dateSysMetadataModified = now
+  sysmeta_pyxb.dateUploaded = now
+  sysmeta_pyxb.formatId = format_id
+  sysmeta_pyxb.identifier = pid
+  sysmeta_pyxb.rightsHolder = generate_random_ascii('rights_holder')
+  sysmeta_pyxb.size = len(sciobj_str)
+  sysmeta_pyxb.submitter = generate_random_ascii('submitter')
+
+  if include_obsolescence_bool:
+    sysmeta_pyxb.obsoletedBy = generate_random_ascii('obsoleted_by_pid')
+    sysmeta_pyxb.obsoletes = generate_random_ascii('obsoletes_pid')
+
+  sysmeta_pyxb.originMemberNode = generate_random_ascii('origin_mn')
+  sysmeta_pyxb.authoritativeMemberNode = generate_random_ascii('auth_mn')
+
+  return sysmeta_pyxb
+
+
+def _generate_public_access_policy(binding):
+  access_policy_pyxb = binding.accessPolicy()
+  access_rule_pyxb = binding.AccessRule()
+  access_rule_pyxb.subject.append(d1_common.const.SUBJECT_PUBLIC)
+  permission_pyxb = binding.Permission('read')
+  access_rule_pyxb.permission.append(permission_pyxb)
+  access_policy_pyxb.append(access_rule_pyxb)
+  return access_policy_pyxb
