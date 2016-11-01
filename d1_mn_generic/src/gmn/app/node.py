ff68dc0c415ec56bc213e973221558e12c00367d
Roger Dahl <git@dahlsys.com>
2016-10-31 22:56:10 -0600

    Refactor Node doc generator to use module instead of class
    Previously used a class just to group Node doc generation functions,
    which isn't very pythonic.

--- d1_mn_generic/src/gmn/app/node.py
+++ d1_mn_generic/src/gmn/app/node.py
@@ -17,8 +17,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""Generate Node document
+"""Generate Node document based on the current setings for GMN.
 """
 
 # Example Node document:
@@ -48,89 +47,98 @@
 from django.conf import settings
 
 # D1.
-import d1_common.types.dataoneTypes_v2_0
+import d1_common.types.dataoneTypes_v1 as v1
+import d1_common.types.dataoneTypes_v2_0 as v2
+import d1_common.util
 
 # App
 
 
-class Node(object):
-  def __init__(self, binding=None):
-    """binding:
-    d1_common.types.dataoneTypes_v1
-    or
-    d1_common.types.dataoneTypes_v2_0
-    """
-    self._binding = binding or d1_common.types.dataoneTypes_v2_0
+def get_pretty_xml(major_version_int=2):
+  return d1_common.util.pretty_xml(_get_pyxb(major_version_int))
 
-  def get_xml_str(self):
-    return self.get().toxml('utf8')
 
-  def get(self):
-    node = self._binding.node()
-    node.identifier = settings.NODE_IDENTIFIER
-    node.name = settings.NODE_NAME
-    node.description = settings.NODE_DESCRIPTION
-    node.baseURL = settings.NODE_BASEURL
-    node.replicate = settings.NODE_REPLICATE
-    node.synchronize = settings.NODE_SYNCHRONIZE
-    node.type = 'mn'
-    node.state = settings.NODE_STATE
-    node.subject.append(self._binding.Subject(settings.NODE_SUBJECT))
-    node.contactSubject.append(self._binding.Subject(settings.NODE_CONTACT_SUBJECT))
-    node.services = self._create_service_list()
-    if settings.NODE_SYNCHRONIZE:
-      node.synchronization = self._create_sync_policy()
-    if settings.TIER >= 4 and settings.NODE_REPLICATE:
-      node.nodeReplicationPolicy = self._create_replication_policy()
-    return node
+def get_pyxb(major_version_int=2):
+  return _get_pyxb(major_version_int)
 
-  def _create_sync_policy(self):
-    schedule = self._binding.Schedule()
-    schedule.year = settings.NODE_SYNC_SCHEDULE_YEAR
-    schedule.mon = settings.NODE_SYNC_SCHEDULE_MONTH
-    schedule.wday = settings.NODE_SYNC_SCHEDULE_WEEKDAY
-    schedule.mday = settings.NODE_SYNC_SCHEDULE_MONTHDAY
-    schedule.hour = settings.NODE_SYNC_SCHEDULE_HOUR
-    schedule.min = settings.NODE_SYNC_SCHEDULE_MINUTE
-    schedule.sec = settings.NODE_SYNC_SCHEDULE_SECOND
-    sync = self._binding.Synchronization()
-    sync.schedule = schedule
-    return sync
 
-  def _create_replication_policy(self):
-    replication = self._binding.nodeReplicationPolicy()
-    if settings.REPLICATION_MAXOBJECTSIZE != -1:
-      replication.maxObjectSize = settings.REPLICATION_MAXOBJECTSIZE
-    if settings.REPLICATION_SPACEALLOCATED != -1:
-      replication.spaceAllocated = settings.REPLICATION_SPACEALLOCATED
-    for allowed_node in settings.REPLICATION_ALLOWEDNODE:
-      replication.allowedNode.append(self._binding.NodeReference(allowed_node))
-    for allowed_object in settings.REPLICATION_ALLOWEDOBJECTFORMAT:
-      replication.allowedObjectFormat.append(
-        self._binding.ObjectFormatIdentifier(allowed_object)
-      )
-    return replication
+def _get_pyxb(major_version_int):
+  assert major_version_int in (1, 2)
+  binding = v1 if major_version_int == 1 else v2
 
-  def _create_service_list(self):
-    service_list = self._binding.services()
-    self._append_services_for_version(service_list, 'v1')
-    self._append_services_for_version(service_list, 'v2')
-    return service_list
+  node_pyxb = binding.node()
+  node_pyxb.identifier = settings.NODE_IDENTIFIER
+  node_pyxb.name = settings.NODE_NAME
+  node_pyxb.description = settings.NODE_DESCRIPTION
+  node_pyxb.baseURL = settings.NODE_BASEURL
+  node_pyxb.replicate = settings.NODE_REPLICATE
+  node_pyxb.synchronize = settings.NODE_SYNCHRONIZE
+  node_pyxb.type = 'mn'
+  node_pyxb.state = settings.NODE_STATE
+  node_pyxb.subject.append(binding.Subject(settings.NODE_SUBJECT))
+  node_pyxb.contactSubject.append(binding.Subject(settings.NODE_CONTACT_SUBJECT))
+  node_pyxb.services = _create_service_list(binding, major_version_int)
+  if settings.NODE_SYNCHRONIZE:
+    node_pyxb.synchronization = _create_sync_policy(binding)
+  if settings.TIER >= 4 and settings.NODE_REPLICATE:
+    node_pyxb.nodeReplicationPolicy = _create_replication_policy(binding)
+  return node_pyxb
 
-  def _append_services_for_version(self, service_list, service_version):
-    if settings.TIER >= 1:
-      self._append_service(service_list, 'MNCore', service_version)
-      self._append_service(service_list, 'MNRead', service_version)
-    if settings.TIER >= 2:
-      self._append_service(service_list, 'MNAuthorization', service_version)
-    if settings.TIER >= 3:
-      self._append_service(service_list, 'MNStorage', service_version)
-    if settings.TIER >= 4:
-      self._append_service(service_list, 'MNReplication', service_version)
 
-  def _append_service(self, service_list, service_name, service_version):
-    service = self._binding.Service()
-    service.name = self._binding.ServiceName(service_name)
-    service.version = self._binding.ServiceVersion(service_version)
-    service.available = True
-    service_list.append(service)
+def _create_sync_policy(binding):
+  schedule_pyxb = binding.Schedule()
+  schedule_pyxb.year = settings.NODE_SYNC_SCHEDULE_YEAR
+  schedule_pyxb.mon = settings.NODE_SYNC_SCHEDULE_MONTH
+  schedule_pyxb.wday = settings.NODE_SYNC_SCHEDULE_WEEKDAY
+  schedule_pyxb.mday = settings.NODE_SYNC_SCHEDULE_MONTHDAY
+  schedule_pyxb.hour = settings.NODE_SYNC_SCHEDULE_HOUR
+  schedule_pyxb.min = settings.NODE_SYNC_SCHEDULE_MINUTE
+  schedule_pyxb.sec = settings.NODE_SYNC_SCHEDULE_SECOND
+  sync = binding.Synchronization()
+  sync.schedule = schedule_pyxb
+  return sync
+
+
+def _create_replication_policy(binding):
+  replication_pyxb = binding.nodeReplicationPolicy()
+  if settings.REPLICATION_MAXOBJECTSIZE != -1:
+    replication_pyxb.maxObjectSize = settings.REPLICATION_MAXOBJECTSIZE
+  if settings.REPLICATION_SPACEALLOCATED != -1:
+    replication_pyxb.spaceAllocated = settings.REPLICATION_SPACEALLOCATED
+  for allowed_node in settings.REPLICATION_ALLOWEDNODE:
+    replication_pyxb.allowedNode.append(binding.NodeReference(allowed_node))
+  for allowed_object in settings.REPLICATION_ALLOWEDOBJECTFORMAT:
+    replication_pyxb.allowedObjectFormat.append(
+      binding.ObjectFormatIdentifier(allowed_object)
+    )
+  return replication_pyxb
+
+
+def _create_service_list(binding, major_version_int):
+  service_list_pyxb = binding.services()
+  service_list_pyxb.extend(_create_service_list_for_version(binding, 'v1'))
+  if major_version_int == 2:
+    service_list_pyxb.extend(_create_service_list_for_version(binding, 'v2'))
+  return service_list_pyxb
+
+
+def _create_service_list_for_version(binding, service_version):
+  service_list = []
+  if settings.TIER >= 1:
+    service_list.append(_create_service(binding, 'MNCore', service_version))
+    service_list.append(_create_service(binding, 'MNRead', service_version))
+  if settings.TIER >= 2:
+    service_list.append(_create_service(binding, 'MNAuthorization', service_version))
+  if settings.TIER >= 3:
+    service_list.append(_create_service(binding, 'MNStorage', service_version))
+  if settings.TIER >= 4:
+    service_list.append(_create_service(binding, 'MNReplication', service_version))
+  return service_list
+
+
+def _create_service(binding, service_name, service_version):
+  service_pyxb = binding.Service()
+  service_pyxb.name = binding.ServiceName(service_name)
+  service_pyxb.version = binding.ServiceVersion(service_version)
+  service_pyxb.available = True
+  return service_pyxb
