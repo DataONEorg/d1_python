4b245b9a859552adb9c19fafc4bdfab5780782f2
Roger Dahl <git@dahlsys.com>
2017-02-28 10:34:57 -0700

    Add logging NullHandler to prevent "no handler found" errors
    This fixes the issue where "no handler found" errors would be printed by
    the library if library clients did not set up logging.

--- d1_common_python/src/d1_common/__init__.py
+++ d1_common_python/src/d1_common/__init__.py
@@ -23,13 +23,14 @@ Shared code for DataONE Python libraries
 
 __version__ = "2.1.0"
 
-__all__ = [
-  'const',
-  'exceptions',
-  'upload',
-  'xmlrunner',
-  'types.exceptions',
-  'types.dataoneTypes',
-  'types.dataoneErrors',
-  'ext.mimeparser',
-]
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+
+try:
+  from logging import NullHandler
+except ImportError:
+  class NullHandler(logging.Handler):
+    def emit(self, record):
+      pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
