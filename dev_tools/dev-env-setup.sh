7160fae25bd63dc79b092ae80f1c58e02d46bbf7
Roger Dahl <git@dahlsys.com>
2017-05-15 22:43:26 -0600

    Add helper for setting the DataONE Python packages up for development

---
+++ dev_tools/dev-env-setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Set the DataONE Python packages up for development.
+
+sudo pip install --upgrade pip
+sudo pip install --upgrade setuptools
+sudo pip install coverage
+
+cd ~/d1-git/d1_python/d1_common_python/src && sudo python setup.py develop
+cd ~/d1-git/d1_python/d1_libclient_python/src && sudo python setup.py develop
+cd ~/d1-git/d1_python/d1_client_cli/src && sudo python setup.py develop
+cd ~/d1-git/d1_python/d1_mn_generic/src/gmn && sudo python ../setup.py develop
+cd ~/d1-git/d1_python/d1_test_utilities_python/src && sudo python setup.py develop
