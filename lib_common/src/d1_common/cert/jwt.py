66d9f74a7c67ecdb9e7d6045ffc5957adad23cd4
Roger Dahl <git@dahlsys.com>
2017-06-20 09:42:53 -0600

    Add new JWT module

---
+++ lib_common/src/d1_common/cert/jwt.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+
+# This work was created by participants in the DataONE project, and is
+# jointly copyrighted by participating institutions in DataONE. For
+# more information on DataONE, see our web site at http://dataone.org.
+#
+#   Copyright 2009-2016 DataONE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""JSON Web Token (JWT) parsing and validation
+
+- bu64: A URL safe flavor of base64 used by JWTs
+- jwt_bu64: A complete JWT consisting of three dot separated bu64 encoded parts:
+(header_bu64, payload_bu64, signature_bu64)
+- jwt_tup: A complete JWT consisting of a tuple of 3 decoded (raw) parts:
+(header_str, payload_str, signature_str)
+
+http://self-issued.info/docs/draft-jones-json-web-token-01.html
+"""
+
+from __future__ import absolute_import
+
+import base64
+import datetime
+import hashlib
+import json
+import logging
+
+import jwt
+
+import d1_common.cert
+import d1_common.cert.x509
+import d1_common.date_time
+
+CLAIM_LIST = [
+  # JSON key, title, is_date
+  ('iss', 'Issuer', False),
+  ('iat', 'Issued At', True),
+  ('exp', 'Expiration Time', True),
+  ('nbf', 'Not Before Time', True),
+  ('aud', 'Audience', False),
+]
+
+
+def get_subject_with_local_validation(jwt_bu64, cert_obj):
+  """Validate the JWT and return the subject it contains.
+
+  The JWT is validated by checking that it was signed with a CN certificate. If
+  validation fails for any reason, errors are logged and None is returned.
+  Possible errors include:
+
+  - A trusted (TLS/SSL) connection could not be made to the CN holding the
+  signing certificate.
+  - The certificate could not be retrieved from the root CN.
+  - The JWT could not be decoded.
+  - The JWT signature signature was invalid.
+  - The JWT claim set contains invalid "Not Before" or "Expiration Time" claims.
+  """
+  try:
+    jwt_dict = validate_and_decode(jwt_bu64, cert_obj)
+  except JwtException as e:
+    return log_jwt_bu64_info(logging.error, str(e), jwt_bu64)
+  try:
+    return jwt_dict['sub']
+  except LookupError:
+    log_jwt_dict_info(logging.error, 'Missing "sub" key', jwt_dict)
+
+
+def get_subject_with_remote_validation(jwt_bu64, base_url):
+  """Same as get_subject_with_local_validation() except that certificate
+  is downloded from the CN
+  """
+  cert_obj = d1_common.cert.x509.download_as_obj(base_url)
+  return get_subject_with_local_validation(jwt_bu64, cert_obj)
+
+
+def get_subject_with_file_validation(jwt_bu64, cert_path):
+  """Same as get_subject_with_local_validation() except that certificate
+  is read from a local PEM file
+  """
+  cert_obj = d1_common.cert.x509.deserialize_pem_file(cert_path)
+  return get_subject_with_local_validation(jwt_bu64, cert_obj)
+
+
+def get_bu64_tup(jwt_bu64):
+  return jwt_bu64.strip().split('.')
+
+
+def get_jwt_tup(jwt_bu64):
+  return [decode_bu64(v) for v in get_bu64_tup(jwt_bu64)]
+
+
+def get_jwt_bu64(jwt_tup):
+  return '.'.join([encode_bu64(v) for v in jwt_tup])
+
+
+def get_jwt_dict(jwt_bu64):
+  jwt_tup = get_jwt_tup(jwt_bu64)
+  try:
+    jwt_dict = json.loads(jwt_tup[0])
+    jwt_dict.update(json.loads(jwt_tup[1]))
+    jwt_dict['_sig_sha1'] = hashlib.sha1(jwt_tup[2]).hexdigest()
+  except TypeError as e:
+    raise JwtException('Decode failed. error="{}"'.format(e))
+  return jwt_dict
+
+
+def validate_and_decode(jwt_bu64, cert_obj):
+  """Return jwt_dict if signature is valid. Else, raise JwtException"""
+  try:
+    return jwt.decode(
+      jwt_bu64.strip(), cert_obj.public_key(), algorithms=['RS256'], verify=True
+    )
+  except jwt.InvalidTokenError as e:
+    raise JwtException('Signature is invalid. error="{}"'.format(str(e)))
+
+
+def log_jwt_dict_info(log, msg_str, jwt_dict):
+  d = ts_to_str(jwt_dict)
+  # Log known items in specific order, then the rest just sorted
+  log_list = ([(b, d.pop(a)) for a, b, c in CLAIM_LIST if a in d] +
+              [(k, d[k]) for k in sorted(d)])
+  map(
+    log,
+    ['{}:'.format(msg_str)] + ['  {}: {}'.format(k, v) for k, v in log_list]
+  )
+
+
+def log_jwt_bu64_info(log, msg_str, jwt_bu64):
+  return log_jwt_dict_info(log, msg_str, get_jwt_dict(jwt_bu64))
+
+
+def ts_to_str(jwt_dict):
+  """Return a new dict where timestamps are replaced with with human readable
+  dates
+  """
+  d = ts_to_dt(jwt_dict)
+  for k, v in d.items():
+    if isinstance(v, datetime.datetime):
+      d[k] = v.isoformat().replace('T', ' ')
+  return d
+
+
+def ts_to_dt(jwt_dict):
+  """Return a new dict where timestamps are replaced with datetime.datetime()
+  objects
+  """
+  d = jwt_dict.copy()
+  for k, v in [v[:2] for v in CLAIM_LIST if v[2]]:
+    if k in jwt_dict:
+      d[k] = d1_common.date_time.ts_to_dt(jwt_dict[k])
+  return d
+
+
+def encode_bu64(arg):
+  s = base64.b64encode(arg)
+  s = s.rstrip('=')
+  s = s.replace('+', '-')
+  s = s.replace('/', '_')
+  return s
+
+
+def decode_bu64(arg):
+  s = arg
+  s = s.replace('-', '+')
+  s = s.replace('_', '/')
+  p = len(s) % 4
+  if p == 0:
+    pass
+  elif p == 2:
+    s += "=="
+  elif p == 3:
+    s += "="
+  else:
+    raise ValueError('Illegal base64url string')
+  return base64.b64decode(s)
+
+
+class JwtException(Exception):
+  pass
---
+++ lib_common/src/d1_common/tests/test_jwt.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This work was created by participants in the DataONE project, and is
+# jointly copyrighted by participating institutions in DataONE. For
+# more information on DataONE, see our web site at http://dataone.org.
+#
+#   Copyright 2009-2016 DataONE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+
+import logging
+
+import freezegun
+import pytest
+
+import d1_common.cert.jwt
+import d1_common.cert.x509
+import d1_common.util
+
+import d1_test.d1_test_case
+
+TOKEN_FILE_NAME_LIST = [
+  'jwt_token_20161004_092911.base64',
+  'jwt_token_20161004_112502.base64',
+  'jwt_token_20161005_204816.base64',
+  'jwt_token_20170612_232523.base64',
+  'jwt_token_20170613_125652.base64',
+  'jwt_token_20170613_163157.base64',
+]
+
+CERT_FILE_NAME_LIST = [
+  'cert_cn_dataone_org_20170517_122900.pem',
+  'cert_cn_stage_test_dataone_org_20170522_155700.pem',
+  'cert_cn_ucsb_1_dataone_org_20120604_191249.pem',
+  'cert_cn_ucsb_1_dataone_org_20150709_180838.pem',
+]
+
+# The only cert/JWT combinations that successfully validate the JWT signature:
+# cert_cn_dataone_org_20170517_122900.pem - jwt_token_20170612_232523.base64
+# cert_cn_dataone_org_20170517_122900.pem - jwt_token_20170613_163157.base64
+# cert_cn_stage_test_dataone_org_20170522_155700.pem - jwt_token_20170613_125652.base64
+
+
+class TestJwt(d1_test.d1_test_case.D1TestCase):
+  def load_sample_cert_jwt_pair(self, cert_file_name, jwt_file_name):
+    cert_pem = self.load_sample(cert_file_name)
+    cert_obj = d1_common.cert.x509.deserialize_pem(cert_pem)
+    # d1_common.cert.x509.log_cert_info(logging.info, 'CERT', cert_obj)
+    jwt_bu64 = self.load_sample(jwt_file_name)
+    # d1_common.cert.jwt.log_jwt_bu64_info(logging.info, 'JWT', jwt_bu64)
+    return cert_obj, jwt_bu64
+
+  @freezegun.freeze_time('2030-01-01')
+  def test_1000(self):
+    """validate_and_decode(): Validation fails after JWT has expired"""
+    cert_obj, jwt_bu64 = self.load_sample_cert_jwt_pair(
+      'cert_cn_dataone_org_20170517_122900.pem',
+      'jwt_token_20170612_232523.base64',
+    )
+    with pytest.raises(d1_common.cert.jwt.JwtException, match='expired'):
+      d1_common.cert.jwt.validate_and_decode(jwt_bu64, cert_obj)
+
+  @freezegun.freeze_time('2011-01-01')
+  def test_1001(self):
+    """validate_and_decode(): Validation succeeds before JWT has expired"""
+    cert_obj, jwt_bu64 = self.load_sample_cert_jwt_pair(
+      'cert_cn_dataone_org_20170517_122900.pem',
+      'jwt_token_20170612_232523.base64',
+    )
+    d1_common.cert.jwt.validate_and_decode(jwt_bu64, cert_obj)
+
+  @freezegun.freeze_time('2011-01-01')
+  def test_1002(self):
+    """validate_and_decode(): Decoded token matches expected"""
+    cert_obj, jwt_bu64 = self.load_sample_cert_jwt_pair(
+      'cert_cn_dataone_org_20170517_122900.pem',
+      'jwt_token_20170612_232523.base64',
+    )
+    jwt_dict = d1_common.cert.jwt.validate_and_decode(jwt_bu64, cert_obj)
+    self.assert_equals_sample(
+      jwt_dict,
+      'jwt_token_decoded_matches_expected',
+    )
+
+  @freezegun.freeze_time('2011-01-01')
+  def test_1003(self):
+    """validate_and_decode(): Validation fails when signed with
+    other cert"""
+    cert_obj, jwt_bu64 = self.load_sample_cert_jwt_pair(
+      'cert_cn_ucsb_1_dataone_org_20120604_191249.pem',
+      'jwt_token_20170612_232523.base64',
+    )
+    with pytest.raises(d1_common.cert.jwt.JwtException):
+      d1_common.cert.jwt.validate_and_decode(jwt_bu64, cert_obj)
+
+  @freezegun.freeze_time('2011-01-01')
+  def test_1004(self):
+    """get_subject_with_local_validation(): After successful validation, returns
+    subject from JWT"""
+    cert_obj, jwt_bu64 = self.load_sample_cert_jwt_pair(
+      'cert_cn_dataone_org_20170517_122900.pem',
+      'jwt_token_20170612_232523.base64',
+    )
+    assert d1_common.cert.jwt.get_subject_with_local_validation(
+      jwt_bu64, cert_obj
+    ) == 'http://orcid.org/0000-0001-8849-7530'
+
+  @freezegun.freeze_time('2011-01-01')
+  def test_1005(self):
+    """get_subject_with_remote_validation(): Receive expected call to
+    getpeercert()"""
+    cert_obj, jwt_bu64 = self.load_sample_cert_jwt_pair(
+      'cert_cn_ucsb_1_dataone_org_20120604_191249.pem',
+      'jwt_token_20170612_232523.base64',
+    )
+    with self.mock_ssl_download(cert_obj) as (mock_connect, mock_getpeercert):
+      d1_common.cert.jwt.get_subject_with_remote_validation(
+        jwt_bu64, 'https://bogus/node/'
+      )
+      mock_connect.assert_called_with(('bogus', 443))
+
+  @freezegun.freeze_time('2011-01-01')
+  def test_1006(self):
+    """get_subject_with_file_validation(): After successful validation, returns
+    subject from JWT"""
+    cert_path = self.get_sample_path('cert_cn_dataone_org_20170517_122900.pem')
+    jwt_bu64 = self.load_sample('jwt_token_20170612_232523.base64')
+    assert d1_common.cert.jwt.get_subject_with_file_validation(
+      jwt_bu64, cert_path
+    ) == 'http://orcid.org/0000-0001-8849-7530'
+
+  @freezegun.freeze_time('2020-01-01')
+  def test_1007(self):
+    """get_subject_with_file_validation(): Fails with expired token
+    """
+    cert_path = self.get_sample_path('cert_cn_dataone_org_20170517_122900.pem')
+    jwt_bu64 = self.load_sample('jwt_token_20170612_232523.base64')
+    assert d1_common.cert.jwt.get_subject_with_file_validation(
+      jwt_bu64, cert_path
+    ) is None
+
+  @freezegun.freeze_time('2021-01-01')
+  def test_1008(self):
+    """log_jwt_bu64_info(): Outputs expected log"""
+    jwt_bu64 = self.load_sample('jwt_token_20170612_232523.base64')
+    with d1_test.d1_test_case.capture_log() as log_stream:
+      d1_common.cert.jwt.log_jwt_bu64_info(logging.info, "test msg", jwt_bu64)
+    self.assert_equals_sample(
+      log_stream.getvalue(), 'jwt_token_log_jwt_bu64_info_expected'
+    )
