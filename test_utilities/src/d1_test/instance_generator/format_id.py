c4ca2cbee70f01c86046fc66d2c47c5fa34fedec
Roger Dahl <git@dahlsys.com>
2017-06-20 09:44:39 -0600

    Add formatId and UserAgent generators to instance generator

---
+++ test_utilities/src/d1_test/instance_generator/format_id.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This work was created by participants in the DataONE project, and is
+# jointly copyrighted by participating institutions in DataONE. For
+# more information on DataONE, see our web site at http://dataone.org.
+#
+#   Copyright 2009-2016 DataONE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Generate random formatId
+"""
+
+from __future__ import absolute_import
+
+import random
+
+import d1_test.d1_test_case
+
+
+class Generate(object):
+  def __init__(self):
+    self._format_id_list = None
+
+  def __call__(self):
+    if self._format_id_list is None:
+      self._format_id_list = [
+        o.formatId
+        for o in d1_test.d1_test_case.D1TestCase.
+        load_sample_xml_to_pyxb('objectFormatList_v2_0.xml').objectFormat
+      ]
+
+    return random.choice(self._format_id_list)
+
+
+generate = Generate()
---
+++ test_utilities/src/d1_test/instance_generator/media_type.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This work was created by participants in the DataONE project, and is
+# jointly copyrighted by participating institutions in DataONE. For
+# more information on DataONE, see our web site at http://dataone.org.
+#
+#   Copyright 2009-2016 DataONE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Generate random MediaType
+"""
+
+from __future__ import absolute_import
+
+import random
+
+import d1_common.types.dataoneTypes
+
+import d1_test.instance_generator.random_data
+
+
+def generate(min_properties=0, max_properties=5):
+  n_properties = random.randint(min_properties, max_properties)
+  if not n_properties:
+    return None
+  media_type_pyxb = d1_common.types.dataoneTypes.MediaType(
+    name='media_type_{}'.
+    format(d1_test.instance_generator.random_data.random_lower_ascii()),
+  )
+  for _ in range(n_properties):
+    property_pyxb = d1_common.types.dataoneTypes.MediaTypeProperty(
+      d1_test.instance_generator.random_data.random_lower_ascii(),
+      name='prop_{}'.
+      format(d1_test.instance_generator.random_data.random_lower_ascii()),
+    )
+    media_type_pyxb.property_.append(property_pyxb)
+  return media_type_pyxb
