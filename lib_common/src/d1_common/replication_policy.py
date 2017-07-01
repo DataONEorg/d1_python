1342db1f4f8dff47fc8cca5ba83c256baf799019
Roger Dahl <git@dahlsys.com>
2017-06-30 20:10:03 -0600

    Add module with utilities for handling the ReplicationPolicy type

---
+++ lib_common/src/d1_common/replication_policy.py
@@ -0,0 +1,216 @@
+# -*- coding: utf-8 -*-
+
+# This work was created by participants in the DataONE project, and is
+# jointly copyrighted by participating institutions in DataONE. For
+# more information on DataONE, see our web site at http://dataone.org.
+#
+#   Copyright 2009-2016 DataONE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Utilities for handling the DataONE NodeReplicationPolicy type
+
+Example rp_pyxb:
+
+<replicationPolicy replicationAllowed="true" numberReplicas="3">
+  <!--Zero or more repetitions:-->
+  <preferredMemberNode>node1</preferredMemberNode>
+  <preferredMemberNode>node2</preferredMemberNode>
+  <preferredMemberNode>node3</preferredMemberNode>
+  <!--Zero or more repetitions:-->
+  <blockedMemberNode>node4</blockedMemberNode>
+  <blockedMemberNode>node5</blockedMemberNode>
+</replicationPolicy>
+
+Example rp_dict:
+
+{
+  'allowed': True,
+  'num': 3,
+  'blockedMemberNode': {'urn:node:NODE1', 'urn:node:NODE2', 'urn:node:NODE3'},
+  'preferredMemberNode': {'urn:node:NODE4', 'urn:node:NODE5'},
+}
+
+"""
+
+from __future__ import absolute_import
+
+import d1_common.types.dataoneTypes
+import d1_common.xml
+
+# Via SysMeta
+
+
+def has_replication_policy(sysmeta_pyxb):
+  return bool(getattr(sysmeta_pyxb, 'replicationPolicy', False))
+
+
+def sysmeta_add_preferred(sysmeta_pyxb, node_urn):
+  """Add to preferred if not already there. Remove from blocked if there
+  """
+  if not has_replication_policy(sysmeta_pyxb):
+    sysmeta_set_default_rp(sysmeta_pyxb)
+  rp_pyxb = sysmeta_pyxb.replicationPolicy
+  add_node(rp_pyxb, 'pref', node_urn)
+  del_node(rp_pyxb, 'block', node_urn)
+
+
+def sysmeta_add_blocked(sysmeta_pyxb, node_urn):
+  """Add to blocked if not already there. Remove from preferred if there
+  """
+  if not has_replication_policy(sysmeta_pyxb):
+    sysmeta_set_default_rp(sysmeta_pyxb)
+  rp_pyxb = sysmeta_pyxb.replicationPolicy
+  add_node(rp_pyxb, 'block', node_urn)
+  del_node(rp_pyxb, 'pref', node_urn)
+
+
+def sysmeta_set_default_rp(sysmeta_pyxb):
+  """Set a default, empty, replicationPolicy. Overwrite any existing rp."""
+  sysmeta_pyxb.replicationPolicy = dict_to_pyxb({
+    'allowed': False,
+    'num': 0,
+    'block': set(),
+    'pref': set()
+  })
+
+
+# Direct ReplicationPolicy
+
+
+def normalize(rp_pyxb):
+  """Normalize a ReplicationPolicy PyXB type in place
+
+  Blocked overrides preferred, so if a {node_urn} is in both, it is removed
+  from preferred.
+  """
+
+  def sort(r, a):
+    d1_common.xml.sort_value_list_pyxb(get(r, a))
+
+  rp_pyxb.preferredMemberNode = (
+    set(get(rp_pyxb, 'pref')) - set(get(rp_pyxb, 'block'))
+  )
+  sort(rp_pyxb, 'block')
+  sort(rp_pyxb, 'pref')
+
+
+def is_preferred(rp_pyxb, node_urn):
+  """Return True if {node_urn} is a preferred replica target
+
+  Blocked overrides preferred, so if a {node_urn} is in both, return False.
+  """
+  return (
+    node_urn in get(rp_pyxb, 'pref') and node_urn not in get(rp_pyxb, 'block')
+  )
+
+
+def is_blocked(rp_pyxb, node_urn):
+  """Return True if {node_urn} is a blocked replica target
+
+  Blocked overrides preferred, so if a {node_urn} is in both, return False.
+  """
+  return node_urn in get(rp_pyxb, 'block')
+
+
+def is_equivalent_pyxb(a_pyxb, b_pyxb):
+  return pyxb_to_dict(a_pyxb) == pyxb_to_dict(b_pyxb)
+
+
+def is_equivalent_xml(a_xml, b_xml):
+  return is_equivalent_pyxb(
+    d1_common.xml.deserialize(a_xml),
+    d1_common.xml.deserialize(b_xml),
+  )
+
+
+def add_preferred(rp_pyxb, node_urn):
+  """Add to preferred if not already there. Remove from blocked if there
+  """
+  add_node(rp_pyxb, 'pref', node_urn)
+  del_node(rp_pyxb, 'block', node_urn)
+
+
+def add_blocked(rp_pyxb, node_urn):
+  """Add to blocked if not already there. Remove from preferred if there
+  """
+  add_node(rp_pyxb, 'block', node_urn)
+  del_node(rp_pyxb, 'pref', node_urn)
+
+
+def add_node(rp_pyxb, attr, node_url):
+  setattr(rp_pyxb, exp(attr), get_vals(rp_pyxb, attr) | {node_url})
+  ensure_allow_rp(rp_pyxb)
+  normalize(rp_pyxb)
+
+
+def del_blocked(rp_pyxb, node_url):
+  del_node(rp_pyxb, 'block', node_url)
+
+
+def del_preferred(rp_pyxb, node_url):
+  del_node(rp_pyxb, 'pref', node_url)
+
+
+def del_node(rp_pyxb, attr, node_url):
+  setattr(rp_pyxb, exp(attr), get_vals(rp_pyxb, attr) - {node_url})
+  normalize(rp_pyxb)
+
+
+def pyxb_to_dict(rp_pyxb):
+  """Returns dict representation of ReplicationPolicy PyXB.
+  """
+  return {
+    'allowed': bool(get(rp_pyxb, 'allowed')),
+    'num': get_n_rep(rp_pyxb),
+    'block': get_vals(rp_pyxb, 'block'),
+    'pref': get_vals(rp_pyxb, 'pref'),
+  }
+
+
+def dict_to_pyxb(rp_dict):
+  rp_pyxb = d1_common.types.dataoneTypes.replicationPolicy()
+  rp_pyxb.replicationAllowed = rp_dict['allowed']
+  rp_pyxb.numberReplicas = rp_dict['num']
+  rp_pyxb.blockedMemberNode = rp_dict['block']
+  rp_pyxb.preferredMemberNode = rp_dict['pref']
+  normalize(rp_pyxb)
+  return rp_pyxb
+
+
+def ensure_allow_rp(rp_pyxb):
+  """Ensure that RP allows replication"""
+  if not rp_pyxb.replicationAllowed:
+    rp_pyxb.replicationAllowed = True
+  if not rp_pyxb.numberReplicas:
+    rp_pyxb.numberReplicas = 3
+
+
+def get(rp_pyxb, attr):
+  return getattr(rp_pyxb, exp(attr), [])
+
+
+def get_vals(rp_pyxb, attr):
+  return {x.value() for x in getattr(rp_pyxb, exp(attr), [])}
+
+
+def get_n_rep(rp_pyxb):
+  return int(getattr(rp_pyxb, exp('num'), 0))
+
+
+def exp(attr):
+  return {
+    'block': 'blockedMemberNode',
+    'pref': 'preferredMemberNode',
+    'allowed': 'replicationAllowed',
+    'num': 'numberReplicas',
+  }[attr]
---
+++ lib_common/src/d1_common/tests/test_replication_policy.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+
+# This work was created by participants in the DataONE project, and is
+# jointly copyrighted by participating institutions in DataONE. For
+# more information on DataONE, see our web site at http://dataone.org.
+#
+#   Copyright 2009-2016 DataONE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+
+import copy
+
+import d1_common.replication_policy
+import d1_common.xml
+
+import d1_test.d1_test_case
+
+
+class TestReplicationPolicy(d1_test.d1_test_case.D1TestCase):
+  sm_with_rp_pyxb = d1_test.sample.load_xml_to_pyxb('systemMetadata_v2_0.xml')
+  sm_without_rp_pyxb = d1_test.sample.load_xml_to_pyxb(
+    'sysmeta_variation_3.xml'
+  )
+  rp_pyxb = d1_test.sample.load_xml_to_pyxb(
+    'systemMetadata_v2_0.xml'
+  ).replicationPolicy
+  rp_swiz_pyxb = d1_test.sample.load_xml_to_pyxb(
+    'systemMetadata_v2_0.swizzled.xml'
+  ).replicationPolicy
+
+  def test_0100(self):
+    """has_replication_policy()"""
+    assert d1_common.replication_policy.has_replication_policy(
+      self.sm_with_rp_pyxb
+    )
+    assert not d1_common.replication_policy.has_replication_policy(
+      self.sm_without_rp_pyxb
+    )
+
+  def test_1000(self):
+    """normalize(): Normalize policy without conflict"""
+    rp_swiz_pyxb = copy.deepcopy(self.rp_swiz_pyxb)
+    self.sample.assert_equals(self.rp_swiz_pyxb, 'normalize_swiz')
+    d1_common.replication_policy.normalize(rp_swiz_pyxb)
+    self.sample.assert_equals(rp_swiz_pyxb, 'normalize_normalized')
+
+  def test_1010(self):
+    """pyxb_to_dict()"""
+    rp_swiz_pyxb = copy.deepcopy(self.rp_swiz_pyxb)
+    rp_dict = d1_common.replication_policy.pyxb_to_dict(rp_swiz_pyxb)
+    self.sample.assert_equals(rp_dict, 'pyxb_to_dict')
+
+  def test_1020(self):
+    """dict_to_pyxb()"""
+    rp_swiz_pyxb = copy.deepcopy(self.rp_swiz_pyxb)
+    rp_dict_1 = d1_common.replication_policy.pyxb_to_dict(rp_swiz_pyxb)
+    rp_pyxb_2 = d1_common.replication_policy.dict_to_pyxb(rp_dict_1)
+    rp_dict_2 = d1_common.replication_policy.pyxb_to_dict(rp_pyxb_2)
+    self.sample.gui_sxs_diff(rp_dict_1, rp_dict_2)
+
+  def test_1030(self):
+    """add_node(): Add node without conflict"""
+    rp_swiz_pyxb = copy.deepcopy(self.rp_swiz_pyxb)
+    d1_common.replication_policy.add_preferred(
+      rp_swiz_pyxb, 'add_preferred_urn'
+    )
+    self.sample.assert_equals(rp_swiz_pyxb, 'add_node')
+
+  def test_1040(self):
+    """add_node(): Add node with conflict"""
+    rp_swiz_pyxb = copy.deepcopy(self.rp_swiz_pyxb)
+    d1_common.replication_policy.add_preferred(rp_swiz_pyxb, 'add_urn')
+    d1_common.replication_policy.add_blocked(rp_swiz_pyxb, 'add_urn')
+    # Assert add_urn not in preferred
+    self.sample.assert_equals(rp_swiz_pyxb, 'add_node_conflict')
+
+  def test_1050(self):
+    """normalize(): Normalize without conflict"""
+    rp_swiz_pyxb = copy.deepcopy(self.rp_swiz_pyxb)
+    self.sample.assert_equals(self.rp_swiz_pyxb, 'normalize_swiz')
+    d1_common.replication_policy.normalize(rp_swiz_pyxb)
+    self.sample.assert_equals(rp_swiz_pyxb, 'normalize_normalized')
+
+  def test_1060(self):
+    """normalize(): Normalize with conflict"""
+    rp_swiz_pyxb = copy.deepcopy(self.rp_swiz_pyxb)
+    rp_dict = d1_common.replication_policy.pyxb_to_dict(rp_swiz_pyxb)
+    rp_dict['pref'].add('add_urn')
+    rp_dict['block'].add('add_urn')
+    rp_pyxb = d1_common.replication_policy.dict_to_pyxb(rp_dict)
+    self.sample.assert_equals(rp_pyxb, 'normalize_swiz_conflict_before')
+    d1_common.replication_policy.normalize(rp_pyxb)
+    self.sample.assert_equals(rp_pyxb, 'normalize_swiz_conflict_after')
+
+  def test_1065(self):
+    """sysmeta_set_default_rp(): Existing RP"""
+    sm_pyxb = copy.deepcopy(self.sm_with_rp_pyxb)
+    d1_common.replication_policy.sysmeta_set_default_rp(sm_pyxb)
+    self.sample.assert_equals(sm_pyxb, 'sysmeta_set_default_rp_existing')
+
+  def test_1066(self):
+    """sysmeta_set_default_rp(): New RP"""
+    sm_pyxb = copy.deepcopy(self.sm_without_rp_pyxb)
+    d1_common.replication_policy.sysmeta_set_default_rp(sm_pyxb)
+    self.sample.assert_equals(sm_pyxb, 'sysmeta_set_default_rp_new')
+
+  def test_1067(self):
+    """add_node(): Add node without existing RP"""
+    sm_pyxb = copy.deepcopy(self.sm_without_rp_pyxb)
+    d1_common.replication_policy.sysmeta_add_preferred(sm_pyxb, 'add_pref_urn')
+    self.sample.assert_equals(sm_pyxb, 'add_node_without_existing')
+
+  def test_1070(self):
+    """sysmeta_add_preferred()"""
+    sm_pyxb = copy.deepcopy(self.sm_with_rp_pyxb)
+    d1_common.replication_policy.sysmeta_add_preferred(sm_pyxb, 'add_urn')
+    d1_common.replication_policy.sysmeta_add_blocked(sm_pyxb, 'add_urn')
+    # Assert add_urn not in preferred
+    self.sample.assert_equals(sm_pyxb, 'sysmeta_add_preferred_conflict')
