d7406f1aa9e7c2fcea8eb8bd0e985060efbfa075
Roger Dahl <git@dahlsys.com>
2018-09-01 19:13:23 -0600

    Add Apache, Postgres, Python version numbers to the GMN home page
    - Also, send only needed values to template, instead of entire locals()

--- gmn/src/d1_gmn/app/views/internal.py
+++ gmn/src/d1_gmn/app/views/internal.py
@@ -25,6 +25,7 @@ be available when the MN is in production.
 
 import ctypes
 import datetime
+import numbers
 import os
 import platform
 import sys
@@ -45,9 +46,7 @@ import d1_common.types.exceptions
 
 import django
 import django.conf
-from django.db.models import Avg
-from django.db.models import Count
-from django.db.models import Sum
+import django.db.models
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 
@@ -57,63 +56,130 @@ def home(request):
   if request.path.endswith('/'):
     return HttpResponseRedirect(request.path[:-1])
 
-  gmn_version = d1_gmn.__version__
-  django_version = ', '.join(map(str, django.VERSION))
+  context_dict = {
+    'node_identifier_str':
+      django.conf.settings.NODE_IDENTIFIER,
+    'node_name':
+      django.conf.settings.NODE_NAME,
+    'gmn_version_str':
+      d1_gmn.__version__,
+    'python_version_str':
+      get_python_version_str(),
+    'django_version_str':
+      ', '.join(map(str, django.VERSION)),
+    'postgres_version_str':
+      get_postgres_version_str(),
+    'avg_sci_data_size_bytes':
+      get_avg_sci_data_size_bytes(),
+    'total_sciobj_count':
+      get_total_sciobj_count(),
+    'total_event_count':
+      get_total_event_count(),
+    'last_hour_event_count':
+      get_last_hour_event_count(),
+    'unique_subject_count':
+      get_unique_subject_count(),
+    'total_permissions_count':
+      get_total_permissions_count(),
+    'server_time':
+      d1_common.date_time.local_now().strftime('%Y-%m-%d %H:%M:%S %Z (%z)'),
+    'sciobj_storage_space_gib_str':
+      '{:.2f} GiB / {:.2f} GiB'.format(
+        float(get_sciobj_storage_used_bytes()) / 1024**3,
+        float(get_obj_store_free_space_bytes()) / 1024**3,
+      ),
+    'sciobj_count_by_format':
+      get_object_count_by_format(),
+    'node_description':
+      django.conf.settings.NODE_DESCRIPTION,
+  }
 
-  n_science_objects = '{:,}'.format(
-    d1_gmn.app.models.ScienceObject.objects.count()
-  )
-
-  avg_sci_data_size_bytes = d1_gmn.app.models.ScienceObject.objects.aggregate(
-    Avg('size')
-  )['size__avg'] or 0
-  avg_sci_data_size = '{:,}'.format(int(avg_sci_data_size_bytes))
-
-  n_objects_by_format = d1_gmn.app.models.ScienceObject.objects.values(
-    'format', 'format__format'
-  ).annotate(dcount=Count('format'))
-
-  n_connections_total = '{:,}'.format(
-    d1_gmn.app.models.EventLog.objects.count()
-  )
-
-  n_connections_in_last_hour = '{:,}'.format(
-    d1_gmn.app.models.EventLog.objects.filter(
-      timestamp__gte=datetime.datetime.utcnow() - datetime.timedelta(hours=1)
-    ).count()
-  )
-
-  n_unique_subjects = '{:,}'.format(d1_gmn.app.models.Subject.objects.count())
-
-  n_storage_used = d1_gmn.app.models.ScienceObject.objects.aggregate(
-    Sum('size')
-  )['size__sum'] or 0
-  n_storage_free = _get_free_space(django.conf.settings.OBJECT_STORE_PATH)
-  storage_space = '{:.2f} GiB / {:.2f} GiB'.format(
-    float(n_storage_used) / 1024**3, float(n_storage_free) / 1024**3
-  )
-
-  n_permissions = '{:,}'.format(d1_gmn.app.models.Permission.objects.count())
-
-  server_time = datetime.datetime.utcnow()
-
-  node_identifier = django.conf.settings.NODE_IDENTIFIER
-  node_name = django.conf.settings.NODE_NAME
-  node_description = django.conf.settings.NODE_DESCRIPTION
+  context_grouped_digits_dict = {
+    k: group_digits(v) if isinstance(v, numbers.Number) else v
+    for k, v in context_dict.items()
+  }
 
   return render_to_response(
-    'home.html', locals(), content_type=d1_common.const.CONTENT_TYPE_XHTML
+    'home.html',
+    context_grouped_digits_dict,
+    content_type=d1_common.const.CONTENT_TYPE_XHTML,
   )
 
 
-def _get_free_space(folder):
-  """Return folder/drive free space (in bytes)
+def get_python_version_str():
+  return ', '.join(map(str, sys.version_info))
+
+
+def get_postgres_version_str():
+  s = str(django.db.connection.cursor().connection.server_version)
+  return ', '.join((s[0], str(int(s[1:3])), str(int(s[3:]))))
+
+
+def get_total_permissions_count():
+  return d1_gmn.app.models.Permission.objects.count()
+
+
+def get_unique_subject_count():
+  return d1_gmn.app.models.Subject.objects.count()
+
+
+def get_total_sciobj_count():
+  return d1_gmn.app.models.ScienceObject.objects.count()
+
+
+def get_avg_sci_data_size_bytes():
+  return int(
+    d1_gmn.app.models.ScienceObject.objects.
+    aggregate(django.db.models.Avg('size'))['size__avg'] or 0
+  )
+
+
+def get_sciobj_storage_used_bytes():
+  return d1_gmn.app.models.ScienceObject.objects.aggregate(
+    django.db.models.Sum('size')
+  )['size__sum'] or 0
+
+
+def get_last_hour_event_count():
+  return d1_gmn.app.models.EventLog.objects.filter(
+    timestamp__gte=datetime.datetime.utcnow() - datetime.timedelta(hours=1)
+  ).count()
+
+
+def get_total_event_count():
+  return d1_gmn.app.models.EventLog.objects.count()
+
+
+def get_object_count_by_format():
+  return d1_gmn.app.models.ScienceObject.objects.values('format__format').annotate(
+    count=django.db.models.Count('format__format')
+  ).order_by('-count')
+
+
+def get_obj_store_free_space_bytes():
+  """Return total free space available on the disk on which the object storage
+  resides (in bytes)
   """
+  obj_store_path = django.conf.settings.OBJECT_STORE_PATH
   if platform.system() == 'Windows':
     free_bytes = ctypes.c_ulonglong(0)
     ctypes.windll.kernel32.GetDiskFreeSpaceExW(
-      ctypes.c_wchar_p(folder), None, None, ctypes.pointer(free_bytes)
+      ctypes.c_wchar_p(obj_store_path), None, None, ctypes.pointer(free_bytes)
     )
     return free_bytes.value
   else:
-    return os.statvfs(folder).f_bfree * os.statvfs(folder).f_frsize
+    return os.statvfs(obj_store_path
+                      ).f_bfree * os.statvfs(obj_store_path).f_frsize
+
+
+def get_os_distribution_description():
+  try:
+    with open('/etc/lsb-release') as f:
+      lsb_dict = dict([s.split('=') for s in f.readlines()])
+      return lsb_dict['DISTRIB_DESCRIPTION'].strip('"\n')
+  except (EnvironmentError, KeyError):
+    return 'Unable to detect OS version'
+
+
+def group_digits(n):
+  return '{:,}'.format(n)
