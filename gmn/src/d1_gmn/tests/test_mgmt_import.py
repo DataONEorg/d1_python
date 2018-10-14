028a51a581f045e6569797cf9e325146401121ae
Roger Dahl <git@dahlsys.com>
2018-10-13 23:41:33 -0600

    Add test for GMN multithreaded bulk importer (ongoing)

--- gmn/src/d1_gmn/tests/test_mgmt_import.py
+++ gmn/src/d1_gmn/tests/test_mgmt_import.py
@@ -19,7 +19,9 @@
 # limitations under the License.
 """Test the bulk importer management command
 """
+import re
 
+import pytest
 import responses
 
 import d1_gmn.tests.gmn_test_case
@@ -31,20 +33,34 @@ import d1_test.mock_api.get_system_metadata as mock_get_system_metadata
 import d1_test.mock_api.list_objects as mock_object_list
 
 
+@pytest.mark.skip('Need to refactor import.py to work with the tests')
+# import.py closes Django db connections, which breaks pytest-django.
 @d1_test.d1_test_case.reproducible_random_decorator('TestMgmtImport')
 class TestMgmtImport(d1_gmn.tests.gmn_test_case.GMNTestCase):
   @responses.activate
-  def test_1000(self, capsys):
+  def test_1000(self, caplog):
     mock_object_list.add_callback(d1_test.d1_test_case.MOCK_REMOTE_BASE_URL)
     mock_log_records.add_callback(d1_test.d1_test_case.MOCK_REMOTE_BASE_URL)
     mock_get_system_metadata.add_callback(
       d1_test.d1_test_case.MOCK_REMOTE_BASE_URL
     )
     mock_get.add_callback(d1_test.d1_test_case.MOCK_REMOTE_BASE_URL)
-
-    self.call_management_command(
-      'import', '--force', '--major=2',
-      d1_test.d1_test_case.MOCK_REMOTE_BASE_URL
-    )
-    stdout, stderr = capsys.readouterr()
-    self.sample.assert_equals(stdout, 'import')
+    with d1_test.d1_test_case.capture_std() as (out_stream, err_stream):
+      self.call_management_command(
+        'import', '--force', '--clear', '--debug', '--workers=1',
+        '--page-size=9', '--major=2', d1_test.d1_test_case.MOCK_REMOTE_BASE_URL
+      )
+    # The importer is multiprocessed but only log output written by the main
+    # process is captured. It's enough to give an indication of successful run
+    # so we leave it at that. Capturing the output from the other processes is
+    # apparently not trivial.
+    #
+    # Due to the multiprocessing, the messages don't look exactly the same each
+    # run, so we strip out the volatile parts before comparing.
+    log_str = d1_test.d1_test_case.get_caplog_text(caplog)
+    log_str = re.sub('Waiting to queue task\n', '', log_str)
+    log_str = re.sub('start', '[START/COUNT]', log_str)
+    log_str = re.sub('count', '[START/COUNT]', log_str)
+    log_str = re.sub('(?:total_run_sec=)[\d.]*', '[SEC]', log_str)
+    log_str = re.sub('(?:total_run_dhm=)[\ddhm"]*', '[DHM]', log_str)
+    self.sample.assert_equals(log_str, 'bulk_import_log')
