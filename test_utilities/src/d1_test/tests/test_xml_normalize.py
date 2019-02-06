2a7da2bea1f2a5df6591ef756e6d4bff229a2dd8
Roger Dahl <git@dahlsys.com>
2019-02-05 20:02:22 -0700

    Add tests for xml_normalize

---
+++ test_utilities/src/d1_test/tests/test_xml_normalize.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This work was created by participants in the DataONE project, and is
+# jointly copyrighted by participating institutions in DataONE. For
+# more information on DataONE, see our web site at http://dataone.org.
+#
+#   Copyright 2009-2016 DataONE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import pytest
+
+import d1_test.d1_test_case
+import d1_test.xml_normalize
+
+
+class TestXmlNormalize(d1_test.d1_test_case.D1TestCase):
+    @pytest.mark.parametrize(
+        "xml_testfile",
+        [
+          'xml_normalize_1.xml',
+          'xml_normalize_2.xml',
+          'systemMetadata_v2_0.swizzled.xml',
+          'node_list_gmn_valid_swizzled.xml',
+        ],
+    )
+    def test_0010(self, xml_testfile):
+      xml = self.test_files.load_xml_to_str(xml_testfile)
+      norm_str = d1_test.xml_normalize.get_normalized_xml_representation(xml)
+      # self.sample.gui_sxs_diff(xml, norm_str)
+      self.sample.assert_equals(
+        norm_str, xml_testfile
+      )
