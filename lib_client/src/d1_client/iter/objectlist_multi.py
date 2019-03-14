b15fe23b03eb125a9620177e8a8be88a37e40c9e
Roger Dahl <git@dahlsys.com>
2019-03-13 20:33:28 -0600

    Move the multiprocessed object iterator to a class

--- lib_client/src/d1_client/iter/objectlist_multi.py
+++ lib_client/src/d1_client/iter/objectlist_multi.py
@@ -55,7 +55,7 @@ class ObjectListIteratorMulti(object):
         self._client_args_dict = client_args_dict or {}
         self._list_objects_args_dict = list_objects_args_dict or {}
         # d1_common.type_conversions.set_default_pyxb_namespace(api_major)
-        self.total = _get_total_object_count(
+        self.total = self._get_total_object_count(
             base_url, api_major, self._client_args_dict, self._list_objects_args_dict
         )
 
@@ -64,7 +64,7 @@ class ObjectListIteratorMulti(object):
         queue = manager.Queue(maxsize=self._max_queue_size)
 
         process = multiprocessing.Process(
-            target=_get_all_pages,
+            target=self._get_all_pages,
             args=(
                 queue,
                 self._base_url,
@@ -81,85 +81,87 @@ class ObjectListIteratorMulti(object):
         while True:
             object_info_pyxb = queue.get()
             if object_info_pyxb is None:
-                self._log.debug('Received None sentinel value. Stopping iteration')
+                self._log.debug("Received None sentinel value. Stopping iteration")
                 break
             yield object_info_pyxb
 
         process.join()
 
+    def _get_total_object_count(
+        self, base_url, api_major, client_args_dict, list_objects_args_dict
+    ):
+        client = self._create_client(base_url, api_major, client_args_dict)
+        args_dict = list_objects_args_dict.copy()
+        args_dict["count"] = 0
+        return client.listObjects(**args_dict).total
 
-def _get_total_object_count(
-    base_url, api_major, client_args_dict, list_objects_args_dict
-):
-    client = _create_client(base_url, api_major, client_args_dict)
-    args_dict = list_objects_args_dict.copy()
-    args_dict['count'] = 0
-    return client.listObjects(**args_dict).total
+    def _get_all_pages(
+        self,
+        queue,
+        base_url,
+        page_size,
+        max_workers,
+        client_args_dict,
+        list_objects_args_dict,
+        n_total,
+    ):
+        self._log.info("Creating pool of {} workers".format(max_workers))
+        pool = multiprocessing.Pool(processes=max_workers)
+        n_pages = (n_total - 1) // page_size + 1
 
-
-def _get_all_pages(
-    queue,
-    base_url,
-    page_size,
-    max_workers,
-    client_args_dict,
-    list_objects_args_dict,
-    n_total,
-):
-    self._log.info('Creating pool of {} workers'.format(max_workers))
-    pool = multiprocessing.Pool(processes=max_workers)
-    n_pages = (n_total - 1) // page_size + 1
-
-    for page_idx in range(n_pages):
-        self._log.debug('apply_async(): page_idx={} n_pages={}'.format(page_idx, n_pages))
-        pool.apply_async(
-            _get_page,
-            args=(
-                queue,
-                base_url,
-                page_idx,
-                n_pages,
-                page_size,
-                client_args_dict,
-                list_objects_args_dict,
-            ),
-        )
-    # Prevent any more tasks from being submitted to the pool. Once all the
-    # tasks have been completed the worker processes will exit.
-    pool.close()
-    # Wait for the worker processes to exit
-    pool.join()
-    # Use None as sentinel value to stop the generator
-    queue.put(None)
-
-
-def _get_page(
-    queue,
-    base_url,
-    page_idx,
-    n_pages,
-    page_size,
-    client_args_dict,
-    list_objects_args_dict,
-):
-    client = d1_client.mnclient_2_0.MemberNodeClient_2_0(base_url, **client_args_dict)
-    try:
-        object_list_pyxb = client.listObjects(
-            start=page_idx * page_size, count=page_size, **list_objects_args_dict
-        )
-        self._log.debug('Retrieved page: {}/{}'.format(page_idx + 1, n_pages))
-        for object_info_pyxb in object_list_pyxb.objectInfo:
-            queue.put(object_info_pyxb)
-    except Exception as e:
-        self._log.error(
-            'Failed to retrieve page: {}/{}. Error: {}'.format(
-                page_idx + 1, n_pages, str(e)
+        for page_idx in range(n_pages):
+            self._log.debug(
+                "apply_async(): page_idx={} n_pages={}".format(page_idx, n_pages)
             )
+            pool.apply_async(
+                self._get_page,
+                args=(
+                    queue,
+                    base_url,
+                    page_idx,
+                    n_pages,
+                    page_size,
+                    client_args_dict,
+                    list_objects_args_dict,
+                ),
+            )
+        # Prevent any more tasks from being submitted to the pool. Once all the
+        # tasks have been completed the worker processes will exit.
+        pool.close()
+        # Wait for the worker processes to exit
+        pool.join()
+        # Use None as sentinel value to stop the generator
+        queue.put(None)
+
+    def _get_page(
+        self,
+        queue,
+        base_url,
+        page_idx,
+        n_pages,
+        page_size,
+        client_args_dict,
+        list_objects_args_dict,
+    ):
+        client = d1_client.mnclient_2_0.MemberNodeClient_2_0(
+            base_url, **client_args_dict
         )
+        try:
+            object_list_pyxb = client.listObjects(
+                start=page_idx * page_size, count=page_size, **list_objects_args_dict
+            )
+            self._log.debug("Retrieved page: {}/{}".format(page_idx + 1, n_pages))
+            for object_info_pyxb in object_list_pyxb.objectInfo:
+                queue.put(object_info_pyxb)
+        except Exception as e:
+            self._log.error(
+                "Failed to retrieve page: {}/{}. Error: {}".format(
+                    page_idx + 1, n_pages, str(e)
+                )
+            )
 
-
-def _create_client(base_url, api_major, client_dict):
-    if api_major in (1, '1', 'v1'):
-        return d1_client.mnclient_1_2.MemberNodeClient_1_2(base_url, **client_dict)
-    else:
-        return d1_client.mnclient_2_0.MemberNodeClient_2_0(base_url, **client_dict)
+    def _create_client(self, base_url, api_major, client_dict):
+        if api_major in (1, "1", "v1"):
+            return d1_client.mnclient_1_2.MemberNodeClient_1_2(base_url, **client_dict)
+        else:
+            return d1_client.mnclient_2_0.MemberNodeClient_2_0(base_url, **client_dict)
