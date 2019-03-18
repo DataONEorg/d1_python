1846e6fe7f6b6a31f7921303556393e7f6fd9845
Roger Dahl <git@dahlsys.com>
2019-03-17 21:25:38 -0600

    Add command to print the syntax tree for a script

---
+++ dev_tools/src/d1_dev/src-print-redbaron-tree.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This work was created by participants in the DataONE project, and is
+# jointly copyrighted by participating institutions in DataONE. For
+# more information on DataONE, see our web site at http://dataone.org.
+#
+#   Copyright 2009-2016 DataONE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import sys
+
+import d1_dev.util
+
+
+def main():
+    """Print the RedBaron syntax tree for a Python module
+    """
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("path", help="Python module path")
+    args = parser.parse_args()
+
+    r = d1_dev.util.redbaron_module_path_to_tree(args.path)
+    print(r.help(True))
+
+if __name__ == "__main__":
+    sys.exit(main())
