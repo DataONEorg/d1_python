35a199dc9c6376c330ef2f6cd9a3503bd57334ee
Roger Dahl <git@dahlsys.com>
2019-04-18 14:08:02 -0600

    Add test for sysmeta_extract module

---
+++ gmn/src/d1_gmn/tests/test_sysmeta_extract.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+# This work was created by participants in the DataONE project, and is
+# jointly copyrighted by participating institutions in DataONE. For
+# more information on DataONE, see our web site at http://dataone.org.
+#
+#   Copyright 2009-2019 DataONE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Test: Extract SciObj information from models."""
+import responses
+
+import d1_gmn.app.sysmeta_extract
+import d1_gmn.tests.gmn_test_case
+
+import d1_test.d1_test_case
+
+
+@d1_test.d1_test_case.reproducible_random_decorator('TestSciObjExtract')
+class TestSciObjExtract(d1_gmn.tests.gmn_test_case.GMNTestCase):
+    @responses.activate
+    def test_1000(self, gmn_client_v2):
+        """Extract single lookup based value, filtered to SID containing "bc"
+        """
+        sciobj_list = d1_gmn.app.sysmeta_extract.extract_values(
+            filter_arg_dict={'pid__chainmember_pid__chain__sid__did__contains': 'bc'},
+            field_list=['formatid'],
+        )
+        self.sample.assert_equals(sciobj_list, 'single_lookup')
+
+    @responses.activate
+    def test_1010(self, gmn_client_v2):
+        """Extract single generate based value, filtered to SID containing "cd"
+        """
+        sciobj_list = d1_gmn.app.sysmeta_extract.extract_values(
+            filter_arg_dict={'pid__chainmember_pid__chain__sid__did__contains': 'cd'},
+            field_list=['permissions'],
+        )
+        self.sample.assert_equals(sciobj_list, 'single_generate')
+
