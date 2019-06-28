2f3b2063b4338b14275c216fe3a742f5e64a8c97
Roger Dahl <git@dahlsys.com>
2019-06-27 23:05:57 -0600

    Add new XML validator that (hopefully) does not download XSDs during validation

---
+++ lib_scimeta/src/d1_scimeta/ext/strip_whitespace.xslt
@@ -0,0 +1,16 @@
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+  <xsl:output method="xml" version="1.0" encoding="UTF-8" indent="yes"/>
+  <xsl:strip-space elements="*"/>
+  <xsl:output indent="yes" method="html"/>
+
+  <xsl:template match="@*|node()">
+    <xsl:copy>
+      <xsl:apply-templates select="@*|node()"/>
+    </xsl:copy>
+  </xsl:template>
+
+  <xsl:template match="text()">
+    <xsl:value-of select="normalize-space(.)"/>
+  </xsl:template>
+
+</xsl:stylesheet>
---
+++ lib_scimeta/src/d1_scimeta/lxml_validate.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import argparse
+import logging
+import sys
+
+import d1_scimeta.xml_schema
+
+import d1_client.command_line
+
+log = logging.getLogger(__name__)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("--formats", action="store_true", help="List valid formatIds")
+    parser.add_argument("--debug", action="store_true", help="Debug level logging")
+    parser.add_argument("xml_path", help="Path to XML file to validate")
+    parser.add_argument(
+        "format_id",
+        nargs="?",
+        default="http://www.isotc211.org/2005/gmd",
+        help="FormatId or short name for the XML file (e.g., eml://ecoinformatics.org/eml-2.1.1",
+    )
+
+    args = parser.parse_args()
+
+    d1_client.command_line.log_setup(is_debug=args.debug)
+
+    if args.formats:
+        d1_scimeta.xml_schema.get_supported_format_id_list()
+        return 0
+
+    try:
+        d1_scimeta.xml_schema.validate(args.format_id, args.xml_path)
+    except d1_scimeta.xml_schema.SciMetaValidationError as e:
+        for line in str(e).splitlines():
+            log.error(line)
+        return 1
+
+    log.info("Validation OK")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
