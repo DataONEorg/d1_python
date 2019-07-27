1a73bdd12bbe504e832d18959f588c2f71d02031
Roger Dahl <git@dahlsys.com>
2019-07-27 16:10:07 -0600

    Add placeholder for adding type hinting using the new syntax in Python.

---
+++ lib_common/src/d1_common/typing.py
@@ -0,0 +1,30 @@
+# This work was created by participants in the DataONE project, and is
+# jointly copyrighted by participating institutions in DataONE. For
+# more information on DataONE, see our web site at http://dataone.org.
+#
+#   Copyright 2009-2019 DataONE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Support for type annotations in d1_common.
+
+Types for DataONE objects are provided along with all the contents of Python's standard
+``typing`` module.
+"""
+
+# noinspection PyUnresolvedReferences
+from typing import *
+
+D1Client = NewType("D1Client", Any)
+
+AsyncD1Client = NewType("AsyncD1Client", Any)
+Checksum = NewType("Checksum", Any)
